5ed7e67a7ec5a1b96d4816b2a830d51f807f8be9
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-26 18:21:56 +0300

    Refactor API config
    Simply having all properties in the same .properties file should be OK too as long as they're exposed in separate @Configuration files for proper distinction of responsibilities. Additionally, at the time of this commit, the current IntelliJ IDEA version has a bit of trouble inspecting custom .properties files.

--- src/main/java/io/github/northmaxdev/coinplot/config/APIConfig.java
+++ src/main/java/io/github/northmaxdev/coinplot/config/APIConfig.java
@@ -4,21 +4,21 @@ package io.github.northmaxdev.coinplot.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-import java.net.URI;
 
 @Configuration
-@PropertySource("classpath:api.properties")
 public class APIConfig {
 
-    @Value("${uri.base}")
-    private String uriBase;
+    @Value("${api.uri.currencies}")
+    private String currenciesURI;
 
-    public URI createCurrenciesEndpointURI() {
-        // This cannot be pre-constructed as uriBase gets injected *after* the constructor. It *can* be cached, though.
-        // However, since currency data is meant to be queried exactly once during an application's lifetime, this is
-        // most likely not needed.
-        return URI.create(uriBase + "currencies");
+    @Value("${api.uri.exchange-rates.time-series.fmt}")
+    private String exchangeRatesURIFormat;
+
+    public String getCurrenciesURI() {
+        return currenciesURI;
+    }
+
+    public String getExchangeRatesURIFormat() {
+        return exchangeRatesURIFormat;
     }
 }
