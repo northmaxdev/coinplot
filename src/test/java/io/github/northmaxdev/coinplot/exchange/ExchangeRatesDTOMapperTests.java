41886ca4d91d65bb16f26f20af7dcdfefa9a4793
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-27 00:15:34 +0300

    Add ExchangeRatesDTOMapper unit tests

---
+++ src/test/java/io/github/northmaxdev/coinplot/exchange/ExchangeRatesDTOMapperTests.java
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: Unlicense
+
+package io.github.northmaxdev.coinplot.exchange;
+
+import io.github.northmaxdev.coinplot.currency.Currency;
+import io.github.northmaxdev.coinplot.currency.CurrencyService;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExchangeRatesDTOMapperTests {
+
+    // Mock the service like this because life is simply too short to be dealing with Mockito and Spring context stuff
+    static final class CurrencyServiceMock implements CurrencyService {
+
+        @Override
+        public Set<Currency> getAvailableCurrencies() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Optional<Currency> getCurrency(@Nullable String threeLetterISOCode) {
+            return Optional.ofNullable(threeLetterISOCode)
+                    .map(s -> new Currency(s, ""));
+        }
+    }
+
+    CurrencyService currencyService = new CurrencyServiceMock();
+
+    @Test
+    void maps() {
+        var dto = new ExchangeRatesDTO("EUR", Map.of(
+                LocalDate.of(2005, 3, 1), Map.of("CHF", 1.5357, "GBP", 0.6879),
+                LocalDate.of(2005, 3, 3), Map.of("CHF", 1.5461, "GBP", 0.6878)
+        ));
+        var mapper = new ExchangeRatesDTOMapper(currencyService);
+
+        Currency eur = new Currency("EUR", "");
+        Currency chf = new Currency("CHF", "");
+        Currency gbp = new Currency("GBP", "");
+        List<ExchangeRate> expected = List.of(
+                new ExchangeRate(eur, chf, LocalDate.of(2005, 3, 1), 1.5357),
+                new ExchangeRate(eur, gbp, LocalDate.of(2005, 3, 1), 0.6879),
+                new ExchangeRate(eur, chf, LocalDate.of(2005, 3, 3), 1.5461),
+                new ExchangeRate(eur, gbp, LocalDate.of(2005, 3, 3), 0.6878)
+        );
+        List<ExchangeRate> actual = mapper.map(dto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
