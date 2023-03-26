ee682e62de3a30a2e1a964c966849acaffb2e9df
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-26 09:40:56 +0300

    Refactor CurrencyService as per previous changes

--- src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyService.java
+++ src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyService.java
@@ -5,6 +5,7 @@ package io.github.northmaxdev.coinplot.currency;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.northmaxdev.coinplot.config.APIConfig;
+import jakarta.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,35 +16,36 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 @Service
 public final class CurrencyService {
 
     private static final Logger LOG = LoggerFactory.getLogger(CurrencyService.class);
 
-    private final CurrencyRepository repository;
     private final APIConfig apiConfig;
     private final HttpClient httpClient;
-    private final ObjectMapper objectMapper;
-    private final CurrencyDTOMapper dtoMapper;
+    private final ObjectMapper jsonParser;
+    private @Nonnull Set<Currency> cache;
 
     @Autowired
-    public CurrencyService(CurrencyRepository repository,
-                           APIConfig apiConfig,
-                           HttpClient httpClient,
-                           ObjectMapper objectMapper,
-                           CurrencyDTOMapper dtoMapper) {
-        this.repository = repository;
+    public CurrencyService(APIConfig apiConfig, HttpClient httpClient, ObjectMapper jsonParser) {
         this.apiConfig = apiConfig;
         this.httpClient = httpClient;
-        this.objectMapper = objectMapper;
-        this.dtoMapper = dtoMapper;
+        this.jsonParser = jsonParser;
+        this.cache = Set.of();
     }
 
-    public List<Currency> getAvailableCurrencies() {
-        if (repository.isEmpty()) {
+    public Set<Currency> getAvailableCurrencies() {
+        fetchIfCacheIsEmpty();
+        return cache;
+    }
+
+    private void fetchIfCacheIsEmpty() {
+        if (cache.isEmpty()) {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(apiConfig.createCurrenciesEndpointURI())
                     .GET()
@@ -52,16 +54,16 @@ public final class CurrencyService {
             try {
                 // TODO: Profile other response content types, maybe byte[] is faster?
                 HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-                Map<String, String> dto = objectMapper.readValue(response.body(), new TypeReference<>(){});
-                List<Currency> currencies = dtoMapper.map(dto);
+                Map<String, String> dto = jsonParser.readValue(response.body(), new TypeReference<>(){});
+                cache = dto.entrySet()
+                        .stream()
+                        .map(entry -> new Currency(entry.getKey(), entry.getValue()))
+                        .collect(toSet());
 
-                repository.saveAll(currencies);
-                LOG.info("Initialized currency database with " + currencies.size() + " entries");
+                LOG.info("Fetched and cached " + cache.size() + " currencies");
             } catch (IOException | InterruptedException e) {
-                LOG.warn("Failed to initialize currency database: " + e);
+                LOG.warn("Failed to initialize currency data: " + e);
             }
         }
-
-        return repository.findAll();
     }
 }
