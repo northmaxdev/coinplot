ed11a3b1274f846a8a80aea8b9e30b48d211b87b
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-29 21:22:31 +0300

    Add label parameter back to CurrencyUIComponents

--- src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyUIComponents.java
+++ src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyUIComponents.java
@@ -6,17 +6,22 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.combobox.ComboBoxBase;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
 public final class CurrencyUIComponents {
 
     private CurrencyUIComponents() {}
 
-    public static ComboBox<Currency> comboBox(@Nonnull CurrencyService service) {
-        return configureComboBox(new ComboBox<>(), service);
+    public static ComboBox<Currency> comboBox(
+            @Nullable String label,
+            @Nonnull CurrencyService service) {
+        return configureComboBox(new ComboBox<>(label), service);
     }
 
-    public static MultiSelectComboBox<Currency> multiSelectComboBox(@Nonnull CurrencyService service) {
-        return configureComboBox(new MultiSelectComboBox<>(), service);
+    public static MultiSelectComboBox<Currency> multiSelectComboBox(
+            @Nullable String label,
+            @Nonnull CurrencyService service) {
+        return configureComboBox(new MultiSelectComboBox<>(label), service);
     }
 
     private static <B extends ComboBoxBase<B, Currency, ?>> B configureComboBox(
