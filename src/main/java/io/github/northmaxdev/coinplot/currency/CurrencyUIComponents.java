33e46e800970a3ae8990e21bec05ea725be611d0
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-29 16:06:29 +0300

    Remove placeholder parameter in CurrencyUIComponents

--- src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyUIComponents.java
+++ src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyUIComponents.java
@@ -6,27 +6,24 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.combobox.ComboBoxBase;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 
 public final class CurrencyUIComponents {
 
     private CurrencyUIComponents() {}
 
     public static ComboBox<Currency> comboBox(@Nonnull CurrencyService service) {
-        return configureComboBox(new ComboBox<>(), service, "Select a currency");
+        return configureComboBox(new ComboBox<>(), service);
     }
 
     public static MultiSelectComboBox<Currency> multiSelectComboBox(@Nonnull CurrencyService service) {
-        return configureComboBox(new MultiSelectComboBox<>(), service, "Select currencies");
+        return configureComboBox(new MultiSelectComboBox<>(), service);
     }
 
     private static <B extends ComboBoxBase<B, Currency, ?>> B configureComboBox(
             @Nonnull B box,
-            @Nonnull CurrencyService service,
-            @Nullable String placeholder) {
+            @Nonnull CurrencyService service) {
         box.setItems(service.getAvailableCurrencies());
         box.setItemLabelGenerator(Currency::name);
-        box.setPlaceholder(placeholder); // TODO: i18n
 
         return box;
     }
