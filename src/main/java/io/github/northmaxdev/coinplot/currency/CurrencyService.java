993708c712b57095ed18a6e05301bd5a896d7a77
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-03-31 23:11:17 +0300

    Migrate CurrencyService::getAvailableCurrencies return type to Collection

--- src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyService.java
+++ src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyService.java
@@ -4,12 +4,12 @@ package io.github.northmaxdev.coinplot.currency;
 
 import jakarta.annotation.Nullable;
 
+import java.util.Collection;
 import java.util.Optional;
-import java.util.Set;
 
 public interface CurrencyService {
 
-    Set<Currency> getAvailableCurrencies();
+    Collection<Currency> getAvailableCurrencies();
 
     Optional<Currency> getCurrency(@Nullable String threeLetterISOCode);
 }
