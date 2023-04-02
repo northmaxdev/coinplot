51484e06b9918496e7b5429d4a19af68f6701087
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-04-02 10:55:27 +0300

    Add default impl for CurrencyService::getCurrency

--- src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyService.java
+++ src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyService.java
@@ -5,11 +5,17 @@ package io.github.northmaxdev.coinplot.currency;
 import jakarta.annotation.Nullable;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Optional;
 
 public interface CurrencyService {
 
     Collection<Currency> getAvailableCurrencies() throws Exception;
 
-    Optional<Currency> getCurrency(@Nullable String threeLetterISOCode) throws Exception;
+    default Optional<Currency> getCurrency(@Nullable String threeLetterISOCode) throws Exception {
+        var availableCurrencies = getAvailableCurrencies();
+        return availableCurrencies.stream()
+                .filter(currency -> Objects.equals(currency.threeLetterISOCode(), threeLetterISOCode))
+                .findFirst();
+    }
 }
