2ec6d252a5fd6816a121fdba05b494dac23811be
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-04-04 23:10:10 +0300

    Add LocalDateRange::isValid

--- src/main/java/io/github/northmaxdev/coinplot/common/core/LocalDateRange.java
+++ src/main/java/io/github/northmaxdev/coinplot/common/core/LocalDateRange.java
@@ -3,14 +3,19 @@
 package io.github.northmaxdev.coinplot.common.core;
 
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
 import java.time.LocalDate;
 
 public record LocalDateRange(@Nonnull LocalDate start, @Nonnull LocalDate end) {
 
     public LocalDateRange {
-        if (!end.isAfter(start)) {
-            throw new IllegalArgumentException("Start >= end");
+        if (!isValid(start, end)) {
+            throw new IllegalArgumentException("Start date must come before end date");
         }
+    }
+
+    public static boolean isValid(@Nullable LocalDate start, @Nullable LocalDate end) {
+        return start != null && end != null && start.isBefore(end);
     }
 }
--- src/test/java/io/github/northmaxdev/coinplot/common/core/LocalDateRangeTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/common/core/LocalDateRangeTests.java
@@ -2,39 +2,71 @@
 
 package io.github.northmaxdev.coinplot.common.core;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class LocalDateRangeTests {
 
-    @Test
-    void throwsIAEIfEndBeforeStart() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            var start = LocalDate.of(2000, 1, 3);
-            var end = LocalDate.of(2000, 1, 2);
-            new LocalDateRange(start, end);
-        });
+    @Nested
+    class Constructor {
+
+        @Test
+        void throwsIAEIfEndBeforeStart() {
+            assertThatIllegalArgumentException().isThrownBy(() -> {
+                var start = LocalDate.of(2000, 1, 3);
+                var end = LocalDate.of(2000, 1, 2);
+                new LocalDateRange(start, end);
+            });
+        }
+
+        @Test
+        void throwsIAEIfEndEqualsStart() {
+            assertThatIllegalArgumentException().isThrownBy(() -> {
+                var start = LocalDate.of(2000, 1, 1);
+                var end = LocalDate.of(2000, 1, 1);
+                new LocalDateRange(start, end);
+            });
+        }
+
+        @Test
+        void doesNotThrowIfEndAfterStart() {
+            assertThatNoException().isThrownBy(() -> {
+                var start = LocalDate.of(2000, 1, 2);
+                var end = LocalDate.of(2000, 1, 3);
+                new LocalDateRange(start, end);
+            });
+        }
     }
 
-    @Test
-    void throwsIAEIfEndEqualsStart() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            var start = LocalDate.of(2000, 1, 1);
-            var end = LocalDate.of(2000, 1, 1);
-            new LocalDateRange(start, end);
-        });
-    }
+    @Nested
+    class RangeCheck {
 
-    @Test
-    void doesNotThrowIfEndAfterStart() {
-        assertThatNoException().isThrownBy(() -> {
-            var start = LocalDate.of(2000, 1, 2);
-            var end = LocalDate.of(2000, 1, 3);
-            new LocalDateRange(start, end);
-        });
+        @ParameterizedTest
+        @MethodSource("provideParamsAndExpectedValues")
+        void paramsReturnExpectedValue(LocalDate start, LocalDate end, boolean expectedValue) {
+            assertThat(LocalDateRange.isValid(start, end)).isEqualTo(expectedValue);
+        }
+
+        Stream<Arguments> provideParamsAndExpectedValues() {
+            return Stream.of(
+                    arguments(LocalDate.EPOCH, null, false),
+                    arguments(null, LocalDate.EPOCH, false),
+                    arguments(null, null, false),
+                    arguments(LocalDate.EPOCH, LocalDate.EPOCH, false),
+                    arguments(LocalDate.MAX, LocalDate.MIN, false),
+                    arguments(LocalDate.MIN, LocalDate.MAX, true)
+            );
+        }
     }
 }
