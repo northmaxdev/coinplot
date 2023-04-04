da12a029c92ab24856341fd3400c6e95461d8acc
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-04-05 01:05:28 +0300

    Refactor ResourceFetchFailureNotification

---
+++ src/main/java/io/github/northmaxdev/coinplot/currency/CurrencyFetchFailureNotification.java
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicense
+
+package io.github.northmaxdev.coinplot.currency;
+
+import io.github.northmaxdev.coinplot.common.ui.AbstractResourceFetchFailureNotification;
+
+public final class CurrencyFetchFailureNotification extends AbstractResourceFetchFailureNotification {
+
+    @Override
+    protected String getTextI18NPropertyKey() {
+        return "resource-fetch-failure-notification.text.currencies";
+    }
+}
