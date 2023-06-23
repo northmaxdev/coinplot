18fd27d14124b51aaef5c68eb2e86e4737fe727e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-23 11:34:01 +0300

    Add APIRequest

---
+++ src/main/java/io/github/northmaxdev/coinplot/common/web/request/APIRequest.java
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.common.web.request;
+
+import jakarta.annotation.Nonnull;
+
+import java.net.URI;
+
+// Not marked @FunctionalInterface as this is *semantically* not a functional interface.
+// Additional methods may be added in the future.
+public interface APIRequest {
+
+    @Nonnull URI toURI();
+}
