a9b0db273312adc60ef5e3781454f4bd3a673915
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-23 15:59:38 +0300

    Add "backend" package and package-info

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/package-info.java
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * This package contains all things backend. It is NOT dependent on the frontend package.
+ */
+package io.github.northmaxdev.coinplot.backend;
