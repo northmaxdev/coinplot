b23756904954ef4e034e2fb000e113348ff0ce6c
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-23 19:22:29 +0300

    AbstractFrankfurterRequest: add comment about equals/hashCode contract in this inheritance hierarchy

--- src/main/java/io/github/northmaxdev/coinplot/backend/request/frankfurter/AbstractFrankfurterRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/request/frankfurter/AbstractFrankfurterRequest.java
@@ -26,4 +26,8 @@ public abstract class AbstractFrankfurterRequest extends AbstractAPIRequest {
     protected final @Nonnull HttpHost getHost() {
         return host;
     }
+
+    // Due to complexities with equals/hashCode contract implementations and unit testing within big inheritance
+    // hierarchies, only the final subclasses actually implement those methods. AbstractFrankfurterRequest explicitly
+    // marks these methods as abstract to force their implementation by the final subclasses.
 }
