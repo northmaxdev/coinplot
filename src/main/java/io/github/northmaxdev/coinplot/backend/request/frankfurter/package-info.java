b8281502d8ec62a48cbe721644c8dad738440d37
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-23 16:34:36 +0300

    Add "backend.request.frankfurter" package and package-info

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/request/frankfurter/package-info.java
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Package for everything that is related to sending requests to the
+ * <a href="https://www.frankfurter.app/">Frankfurter web API</a>.
+ */
+package io.github.northmaxdev.coinplot.backend.request.frankfurter;
