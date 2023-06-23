97640a9923a53a0d75e91aac4e401a7e0fcaa3c8
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-23 16:04:55 +0300

    Add "backend.response" package and package-info

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/response/package-info.java
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Package for everything that is related to receiving responses from other web services.
+ */
+package io.github.northmaxdev.coinplot.backend.response;
