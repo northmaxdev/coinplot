f65f1c16d54a9b8213d611a182afb668cc993728
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-27 23:20:01 +0300

    Add "backend.request.currencyapi" package and package-info

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/request/currencyapi/package-info.java
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Package for everything that is related to sending requests to
+ * <a href="https://currencyapi.com/">currencyapi.com</a>.
+ */
+package io.github.northmaxdev.coinplot.backend.request.currencyapi;
