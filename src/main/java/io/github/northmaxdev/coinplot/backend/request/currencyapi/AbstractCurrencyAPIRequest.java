9afe4a2d46325a405fd92b069fd966ee640c425a
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-28 22:39:09 +0300

    Add AbstractCurrencyAPIRequest

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/request/currencyapi/AbstractCurrencyAPIRequest.java
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.request.currencyapi;
+
+import io.github.northmaxdev.coinplot.backend.request.AbstractAPIRequest;
+import jakarta.annotation.Nonnull;
+import org.apache.hc.core5.http.HttpHost;
+
+import java.util.List;
+
+import static org.apache.hc.core5.http.URIScheme.HTTPS;
+
+public abstract class AbstractCurrencyAPIRequest extends AbstractAPIRequest {
+
+    private static final HttpHost HOST = new HttpHost(HTTPS.getId(), "api.currencyapi.com");
+
+    @Override
+    protected final @Nonnull HttpHost getHost() {
+        return HOST;
+    }
+
+    @Override
+    protected final @Nonnull List<String> getPathSegments() {
+        // TODO: This can be precomputed once
+        return List.of("v3", getEndpoint());
+    }
+
+    protected abstract @Nonnull String getEndpoint();
+}
