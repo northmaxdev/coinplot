feaa9d4727673f00e9d104cc66f910ffe48a07ca
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-06-29 23:48:55 +0300

    Add AbstractCurrencyAPIRequest::createPathSegments

--- src/main/java/io/github/northmaxdev/coinplot/backend/request/currencyapi/AbstractCurrencyAPIRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/request/currencyapi/AbstractCurrencyAPIRequest.java
@@ -6,6 +6,8 @@ import io.github.northmaxdev.coinplot.backend.request.AbstractAPIRequest;
 import jakarta.annotation.Nonnull;
 import org.apache.hc.core5.http.HttpHost;
 
+import java.util.List;
+
 import static org.apache.hc.core5.http.URIScheme.HTTPS;
 
 public sealed abstract class AbstractCurrencyAPIRequest extends AbstractAPIRequest
@@ -16,5 +18,11 @@ public sealed abstract class AbstractCurrencyAPIRequest extends AbstractAPIReque
     @Override
     protected final @Nonnull HttpHost getHost() {
         return HOST;
+    }
+
+    // Since the mappings of the only endpoints we require follow the same non-changing structure, we can simply create
+    // a static class constant for every subclass's getPathSegments(). This method is intended just for that.
+    protected static List<String> createPathSegments(@Nonnull String endpoint) {
+        return List.of("v3", endpoint);
     }
 }
