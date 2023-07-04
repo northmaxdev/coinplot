c040abc881c0e9f182b27ed86bdcfd029e49a73c
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-04 22:08:21 +0300

    Add CurrencyAPICurrenciesDTO

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/currency/CurrencyAPICurrenciesDTO.java
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.currency;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.Map;
+
+public record CurrencyAPICurrenciesDTO(Map<String, CurrencyData> data) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public record CurrencyData(String symbol, String name, String symbolNative) {}
+}
