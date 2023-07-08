f1480015f2284e6ec96aa5be31078a79aa8cad36
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-08 13:49:06 +0300

    Add ExchangeRatesRequest

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/exchange/ExchangeRatesRequest.java
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.exchange;
+
+import io.github.northmaxdev.coinplot.backend.web.request.APIRequest;
+
+public interface ExchangeRatesRequest extends APIRequest {
+}
