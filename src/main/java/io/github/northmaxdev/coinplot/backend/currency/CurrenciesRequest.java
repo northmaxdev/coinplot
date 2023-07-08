651f52b4a9cbcedfe66d17d5dd20e2975b1a7f7b
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-08 13:47:20 +0300

    Add CurrenciesRequest

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/currency/CurrenciesRequest.java
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.currency;
+
+import io.github.northmaxdev.coinplot.backend.web.request.APIRequest;
+
+public interface CurrenciesRequest extends APIRequest {
+}
