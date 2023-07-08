70bea64b86eb6d03048f291ac695b610f8a3e213
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-08 12:26:16 +0300

    Add ResourceFetchException

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/ResourceFetchException.java
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend;
+
+public final class ResourceFetchException extends RuntimeException {
+
+    public ResourceFetchException(Throwable cause) {
+        super(cause);
+    }
+}
