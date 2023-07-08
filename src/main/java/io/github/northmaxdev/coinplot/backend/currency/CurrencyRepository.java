d8cd41239d977824d3cafb5fef062b6c978b095b
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-08 17:33:08 +0300

    Use JPQL for CurrencyRepository::findAllAsSet

--- src/main/java/io/github/northmaxdev/coinplot/backend/currency/CurrencyRepository.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/currency/CurrencyRepository.java
@@ -3,8 +3,14 @@
 package io.github.northmaxdev.coinplot.backend.currency;
 
 import io.github.northmaxdev.coinplot.backend.db.ExtendedJPARepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface CurrencyRepository extends ExtendedJPARepository<Currency, String> {
+
+    @Query("SELECT c FROM Currency c")
+    Set<Currency> findAllAsSet();
 }
