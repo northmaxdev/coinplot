cd50379dea58e2bb50239cba5a77a18895b7edc2
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-15 15:43:00 +0300

    Add FixerCurrencySetRequest::toURI unit test

--- src/test/java/io/github/northmaxdev/coinplot/backend/fixer/FixerCurrencySetRequestTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/backend/fixer/FixerCurrencySetRequestTests.java
@@ -5,7 +5,21 @@ package io.github.northmaxdev.coinplot.backend.fixer;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class FixerCurrencySetRequestTests {
+
+    @Test
+    void actualURIEqualsExpected() {
+        var request = new FixerCurrencySetRequest("5ac5355b84894ede056ab81b324c4675");
+
+        URI expected = URI.create("https://data.fixer.io/api/symbols?access_key=5ac5355b84894ede056ab81b324c4675");
+        URI actual = request.toURI();
+
+        assertThat(actual).isEqualTo(expected);
+    }
 
     @Test
     void equalsAndHashCode() {
