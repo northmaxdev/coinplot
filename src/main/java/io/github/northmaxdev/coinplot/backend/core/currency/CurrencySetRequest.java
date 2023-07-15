6f730405e4ccd96248f2e70f83116f7b1fa53fb5
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-15 14:49:51 +0300

    Rename and move CurrenciesRequest

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/currency/CurrencySetRequest.java
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.currency;
+
+import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
+
+public interface CurrencySetRequest extends APIRequest {
+}
--- src/main/java/io/github/northmaxdev/coinplot/backend/currency/CurrenciesRequest.java
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-package io.github.northmaxdev.coinplot.backend.currency;
-
-import io.github.northmaxdev.coinplot.backend.web.request.APIRequest;
-
-public interface CurrenciesRequest extends APIRequest {
-}
