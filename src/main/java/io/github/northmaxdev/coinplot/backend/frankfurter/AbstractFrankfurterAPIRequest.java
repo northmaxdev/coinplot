2c8c226021bb38e747a714e08ea06466b99063e6
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-15 15:45:38 +0300

    Update Frankfurter request impl

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/AbstractFrankfurterAPIRequest.java
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.frankfurter;
+
+import io.github.northmaxdev.coinplot.backend.core.web.request.AbstractAPIRequest;
+import jakarta.annotation.Nonnull;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.NameValuePair;
+
+import java.util.Optional;
+
+import static org.apache.hc.core5.http.URIScheme.HTTPS;
+
+public abstract class AbstractFrankfurterAPIRequest extends AbstractAPIRequest {
+
+    private static final HttpHost PUBLIC_INSTANCE_HOST = new HttpHost(HTTPS.getId(), "api.frankfurter.app");
+
+    private final HttpHost host;
+
+    protected AbstractFrankfurterAPIRequest() {
+        this(PUBLIC_INSTANCE_HOST);
+    }
+
+    protected AbstractFrankfurterAPIRequest(@Nonnull HttpHost customHost) {
+        this.host = customHost;
+    }
+
+    @Override
+    protected final @Nonnull HttpHost getHost() {
+        return host;
+    }
+
+    @Override
+    protected final Optional<String> getRootPathSegment() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected final Optional<NameValuePair> getAccessKeyParameter() {
+        return Optional.empty();
+    }
+}
