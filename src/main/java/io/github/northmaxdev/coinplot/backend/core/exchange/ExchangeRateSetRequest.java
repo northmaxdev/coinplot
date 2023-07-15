57e6c159ddf61b151053e8bcea5ab85863e93adb
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-15 15:54:43 +0300

    Move and rename ExchangeRatesRequest

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.exchange;
+
+import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
+
+public interface ExchangeRateSetRequest extends APIRequest {
+}
--- src/main/java/io/github/northmaxdev/coinplot/backend/exchange/ExchangeRatesRequest.java
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-package io.github.northmaxdev.coinplot.backend.exchange;
-
-import io.github.northmaxdev.coinplot.backend.web.request.APIRequest;
-
-public interface ExchangeRatesRequest extends APIRequest {
-}
