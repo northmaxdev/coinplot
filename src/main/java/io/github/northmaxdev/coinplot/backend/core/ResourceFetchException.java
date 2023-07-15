97275fab278bf23c9da58889496af7e7ad375cb4
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-15 20:11:12 +0300

    Add ResourceFetchException(String) ctor

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/ResourceFetchException.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/ResourceFetchException.java
@@ -4,6 +4,10 @@ package io.github.northmaxdev.coinplot.backend.core;
 
 public final class ResourceFetchException extends RuntimeException {
 
+    public ResourceFetchException(String message) {
+        super(message);
+    }
+
     public ResourceFetchException(Throwable cause) {
         super(cause);
     }
