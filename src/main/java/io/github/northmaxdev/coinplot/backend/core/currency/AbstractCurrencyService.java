e4be489d8e9b605641cabebba8fe37cebdcc2483
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-16 19:42:11 +0300

    Add AbstractCurrencyService

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/currency/AbstractCurrencyService.java
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.currency;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.northmaxdev.coinplot.backend.core.ResourceFetchException;
+import io.github.northmaxdev.coinplot.backend.core.web.AbstractHTTPService;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+
+import java.net.http.HttpClient;
+import java.util.Optional;
+import java.util.Set;
+
+public abstract class AbstractCurrencyService<R extends CurrencySetRequest, D>
+        extends AbstractHTTPService<R, D, Set<Currency>>
+        implements CurrencyService {
+
+    private final CurrencyRepository repository;
+
+    protected AbstractCurrencyService(
+            HttpClient httpClient,
+            ObjectMapper jsonParser,
+            CurrencySetDTOMapper<D> dtoMapper,
+            CurrencyRepository repository) {
+        super(httpClient, jsonParser, dtoMapper);
+        this.repository = repository;
+    }
+
+    @Override
+    public final Set<Currency> getAvailableCurrencies() throws ResourceFetchException {
+        fetchIfEmptyRepository();
+        return repository.findAllAsSet();
+    }
+
+    @Override
+    public final Optional<Currency> getCurrency(@Nullable String code) throws ResourceFetchException {
+        fetchIfEmptyRepository();
+        return repository.findByIDNullSafely(code);
+    }
+
+    protected abstract @Nonnull R createAPIRequest();
+
+    private void fetchIfEmptyRepository() throws ResourceFetchException {
+        if (repository.isEmpty()) {
+            R apiRequest = createAPIRequest();
+            Set<Currency> fetchedCurrencies = fetch(apiRequest);
+            repository.saveAll(fetchedCurrencies);
+        }
+    }
+}
