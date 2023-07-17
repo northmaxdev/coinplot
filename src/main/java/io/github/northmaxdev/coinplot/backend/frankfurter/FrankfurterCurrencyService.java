9e71e80b040cc5b935dff791bea402bd33a9f49e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-17 22:35:04 +0300

    Add FrankfurterCurrencyService

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/FrankfurterCurrencyService.java
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.frankfurter;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.northmaxdev.coinplot.backend.core.currency.AbstractCurrencyService;
+import io.github.northmaxdev.coinplot.backend.core.currency.CurrencyRepository;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import org.apache.hc.core5.http.HttpHost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.util.Map;
+
+@Service
+public final class FrankfurterCurrencyService
+        extends AbstractCurrencyService<FrankfurterCurrencySetRequest, Map<String, String>> {
+
+    private static final TypeReference<Map<String, String>> DTO_TYPE_REF = new TypeReference<>() {};
+
+    private @Nullable HttpHost customHost;
+
+    @Autowired
+    public FrankfurterCurrencyService(
+            HttpClient httpClient,
+            ObjectMapper jsonParser,
+            CurrencyRepository repository) {
+        super(httpClient, jsonParser, new FrankfurterCurrencySetDTOMapper(), repository);
+        this.customHost = null;
+    }
+
+    public void setCustomHost(@Nullable HttpHost customHost) {
+        this.customHost = customHost;
+    }
+
+    public void setDefaultHost() {
+        setCustomHost(null);
+    }
+
+    @Override
+    protected @Nonnull FrankfurterCurrencySetRequest createAPIRequest() {
+        return customHost == null
+                ? new FrankfurterCurrencySetRequest()
+                : new FrankfurterCurrencySetRequest(customHost);
+    }
+
+    @Override
+    protected Map<String, String> parseResponseBody(byte[] responseBody, ObjectMapper jsonParser) throws IOException {
+        return jsonParser.readValue(responseBody, DTO_TYPE_REF);
+    }
+}
