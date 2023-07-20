3d71ac8c2c06209a36827db54e4a5aa84f53aeba
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-20 17:34:13 +0300

    Implement test method display name generation

--- src/test/java/io/github/northmaxdev/coinplot/HumanReadableDisplayNameGenerator.java
+++ src/test/java/io/github/northmaxdev/coinplot/HumanReadableDisplayNameGenerator.java
@@ -5,8 +5,16 @@ package io.github.northmaxdev.coinplot;
 import org.junit.jupiter.api.DisplayNameGenerator;
 
 import java.lang.reflect.Method;
+import java.util.Map;
+
+import static java.util.Map.entry;
 
 public final class HumanReadableDisplayNameGenerator implements DisplayNameGenerator {
+
+    private static final Map<String, String> MAGIC_METHOD_NAMES = Map.ofEntries(
+            entry("eq", "equals/hashCode contract"),
+            entry("reqURI", "APIRequest actual URI equals to expected")
+    );
 
     @Override
     public String generateDisplayNameForClass(Class<?> aClass) {
@@ -27,10 +35,11 @@ public final class HumanReadableDisplayNameGenerator implements DisplayNameGener
 
     @Override
     public String generateDisplayNameForMethod(Class<?> aClass, Method method) {
-        throw new UnsupportedOperationException(); // TODO
+        String methodName = method.getName();
+        return MAGIC_METHOD_NAMES.getOrDefault(methodName, markedAsIs(methodName));
     }
 
-    private String markedAsIs(String s) {
+    private static String markedAsIs(String s) {
         // There's no conventional syntax for the purpose of this method; Just do whatever.
         return '<' + s + '>';
     }
