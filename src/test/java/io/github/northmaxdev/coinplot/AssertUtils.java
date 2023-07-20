ae78879047bdbc4e5dc7acab1e50b91e3cb08399
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-20 17:55:56 +0300

    Add util method for APIRequest headers' assertions

--- src/test/java/io/github/northmaxdev/coinplot/AssertUtils.java
+++ src/test/java/io/github/northmaxdev/coinplot/AssertUtils.java
@@ -5,6 +5,7 @@ package io.github.northmaxdev.coinplot;
 import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
 
 import java.net.URI;
+import java.util.Map;
 import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,6 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class AssertUtils {
 
     private AssertUtils() {}
+
+    ////////////////////////////////////////
+    // General-purpose APIRequest asserts //
+    ////////////////////////////////////////
 
     public static <R extends APIRequest> void assertAPIRequestURIEqualsExpected(
             String expectedURILiteral,
@@ -22,5 +27,15 @@ public final class AssertUtils {
         URI actual = request.getURI();
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    public static <R extends APIRequest> void assertAPIRequestHeadersContainOnlyExpected(
+            Map<String, String> expected,
+            Supplier<R> requestSupplier) {
+        R request = requestSupplier.get();
+
+        Map<String, String> actual = request.getHeaders();
+
+        assertThat(actual).containsExactlyInAnyOrderEntriesOf(expected);
     }
 }
