3658710475fd6f1dc4fb01c34cc6929da23d10c9
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-23 00:25:21 +0300

    Add LocalDateInterval::reverse

--- src/main/java/io/github/northmaxdev/coinplot/lang/LocalDateInterval.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/LocalDateInterval.java
@@ -6,9 +6,17 @@ import jakarta.annotation.Nonnull;
 
 import java.time.LocalDate;
 
-public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end) {
+public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end) { // Both inclusive
 
     public boolean isReversed() {
         return start.isAfter(end);
+    }
+
+    public LocalDateInterval reverse() {
+        if (start.isEqual(end)) {
+            return this;
+        }
+
+        return new LocalDateInterval(end, start);
     }
 }
