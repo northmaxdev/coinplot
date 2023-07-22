318df35e222f4bc5a38ce83790a2e58d840a9621
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-22 14:40:03 +0300

    ExchangeRateSetRequest: add common properties

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
@@ -2,7 +2,20 @@
 
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
+import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
 
-public interface ExchangeRateSetRequest extends APIRequest {
+import java.util.Optional;
+import java.util.Set;
+
+// T represents the exact temporal type used for specifying start/end boundaries.
+public interface ExchangeRateSetRequest<T> extends APIRequest {
+
+    Optional<Currency> getBase();
+
+    Set<Currency> getTargets();
+
+    Optional<T> getStart();
+
+    Optional<T> getEnd();
 }
