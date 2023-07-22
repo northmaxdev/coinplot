c04932b3a49803b04f89e24a29936b8db98153d0
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-22 13:44:25 +0300

    Add TestUtils::verifyAPIRequestEquals

--- src/test/java/io/github/northmaxdev/coinplot/TestUtils.java
+++ src/test/java/io/github/northmaxdev/coinplot/TestUtils.java
@@ -4,6 +4,8 @@ package io.github.northmaxdev.coinplot;
 
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 
 import java.net.URI;
 import java.util.Map;
@@ -45,5 +47,20 @@ public final class TestUtils {
         Map<String, String> actual = request.getHeaders();
 
         assertThat(actual).containsExactlyInAnyOrderEntriesOf(expected);
+    }
+
+    public static <R extends APIRequest> void verifyAPIRequestEquals(Class<R> requestClass) {
+        EqualsVerifier.forClass(requestClass)
+                // The class hierarchy of APIRequest implementations mixes between subclasses that add state and
+                // subclasses that merely add behavior. Running an EqualsVerifier with standard configuration leads to
+                // "subclass does not equal superclass" errors. Configuring withRedefinedSuperclass() leads to "subclass
+                // equals superclass but should not" errors. The combination of the former and latter is very weird
+                // considering that we're talking about the same equals/hashCode implementations being tested.
+                // Suppressing such strict inheritance checks passes the tests, but I'm not sure if this is actually
+                // appropriate for the issue at hand considering its documentation, so this may or may not lead to
+                // subtle bugs later on (hopefully not). In the meantime, the tested equals/hashCode implementations
+                // look completely OK.
+                .suppress(Warning.STRICT_INHERITANCE)
+                .verify();
     }
 }
