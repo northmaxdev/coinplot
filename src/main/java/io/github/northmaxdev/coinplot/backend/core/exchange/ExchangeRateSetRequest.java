d7153f0bc922130beb9094769f4ccfaa103c76df
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-22 15:12:58 +0300

    Add ExchangeRateSetRequest::getJoinedTargetCodes

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
@@ -5,8 +5,11 @@ package io.github.northmaxdev.coinplot.backend.core.exchange;
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+
+import static java.util.stream.Collectors.joining;
 
 // T represents the exact temporal type used for specifying start/end boundaries.
 public interface ExchangeRateSetRequest<T> extends APIRequest {
@@ -14,6 +17,24 @@ public interface ExchangeRateSetRequest<T> extends APIRequest {
     Optional<Currency> getBase();
 
     Set<Currency> getTargets();
+
+    // Just a common operation, used mostly as an implementation detail.
+    default Optional<String> getJoinedTargetCodes() {
+        Set<Currency> targets = getTargets();
+
+        if (targets.isEmpty()) {
+            return Optional.empty();
+        }
+
+        String s = targets.stream()
+                .filter(Objects::nonNull) // Just in case
+                .map(Currency::getCode)
+                .collect(joining(","));
+
+        return s.isEmpty()
+                ? Optional.empty()
+                : Optional.of(s);
+    }
 
     Optional<T> getStart();
 
