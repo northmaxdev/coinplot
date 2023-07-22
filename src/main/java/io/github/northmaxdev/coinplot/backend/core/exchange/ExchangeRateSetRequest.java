6d36706612291bcd0b36905c7f219e436b5ee74f
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-23 00:23:48 +0300

    Use LocalDateInterval in ExchangeRateSetRequest

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
@@ -4,18 +4,17 @@ package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
+import io.github.northmaxdev.coinplot.lang.LocalDateInterval;
+import jakarta.annotation.Nonnull;
 
 import java.util.Optional;
 import java.util.Set;
 
-// T represents the exact temporal type used for specifying start/end boundaries.
-public interface ExchangeRateSetRequest<T> extends APIRequest {
+public interface ExchangeRateSetRequest extends APIRequest {
 
     Optional<Currency> getBase();
 
     Set<Currency> getTargets();
 
-    Optional<T> getStart();
-
-    Optional<T> getEnd();
+    @Nonnull LocalDateInterval getDateInterval();
 }
