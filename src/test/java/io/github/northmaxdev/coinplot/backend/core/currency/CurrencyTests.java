8b5ebe6de408e0f8ade3f38a20a4e84184a448f0
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-22 13:56:25 +0300

    Use TestUtils::verifyJPAEntityEquals in Currency equals/hashCode tests

--- src/test/java/io/github/northmaxdev/coinplot/backend/core/currency/CurrencyTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/backend/core/currency/CurrencyTests.java
@@ -2,16 +2,14 @@
 
 package io.github.northmaxdev.coinplot.backend.core.currency;
 
-import nl.jqno.equalsverifier.EqualsVerifier;
-import nl.jqno.equalsverifier.Warning;
 import org.junit.jupiter.api.Test;
+
+import static io.github.northmaxdev.coinplot.TestUtils.verifyJPAEntityEquals;
 
 class CurrencyTests {
 
     @Test
     void eq() {
-        EqualsVerifier.forClass(Currency.class)
-                .suppress(Warning.SURROGATE_KEY) // To only compare entity IDs for equality
-                .verify();
+        verifyJPAEntityEquals(Currency.class);
     }
 }
