1a958cf38b2f074fb5ab973ee415ece24a47e1df
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-25 23:31:38 +0300

    Add ExchangeRateRepository

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.exchange;
+
+import io.github.northmaxdev.coinplot.backend.core.db.ExtendedJPARepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeRateRepository extends ExtendedJPARepository<ExchangeRate, Exchange> {
+}
