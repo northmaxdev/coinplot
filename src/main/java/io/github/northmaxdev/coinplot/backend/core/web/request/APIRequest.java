a8a74d339eb95edad7b620e6ac26aaf2a8437d10
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-07-30 17:42:51 +0300

    Add APIRequest::toHTTPRequestBuilder

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/web/request/APIRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/web/request/APIRequest.java
@@ -5,6 +5,7 @@ package io.github.northmaxdev.coinplot.backend.core.web.request;
 import jakarta.annotation.Nonnull;
 
 import java.net.URI;
+import java.net.http.HttpRequest;
 import java.util.Map;
 
 // Semantically not a @FunctionalInterface
@@ -14,5 +15,15 @@ public interface APIRequest {
 
     default Map<String, String> getHeaders() {
         return Map.of();
+    }
+
+    default HttpRequest.Builder toHTTPRequestBuilder() {
+        URI uri = getURI();
+        Map<String, String> headers = getHeaders();
+
+        HttpRequest.Builder builder = HttpRequest.newBuilder(uri);
+        headers.forEach(builder::setHeader);
+
+        return builder;
     }
 }
