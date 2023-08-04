c56a527a5c9cbbba3766a8fc61219eb46593a2ed
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-05 02:42:18 +0300

    Add MainView (WIP)

---
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/MainView.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.frontend;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route
+@PageTitle("CoinPlot")
+public final class MainView extends AppLayout {
+
+    public MainView() {
+    }
+}
