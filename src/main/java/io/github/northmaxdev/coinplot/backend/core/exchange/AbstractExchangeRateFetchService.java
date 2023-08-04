b1adbb777ec7c39a66e50bc359df784cea8571ea
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-05 02:41:41 +0300

    Add AbstractExchangeRateFetchService (WIP)

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/AbstractExchangeRateFetchService.java
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.exchange;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.northmaxdev.coinplot.backend.core.ResourceFetchFailureException;
+import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
+import io.github.northmaxdev.coinplot.backend.core.web.AbstractRemoteResourceFetchService;
+import io.github.northmaxdev.coinplot.backend.core.web.RemoteResourceFetchFailureException;
+import io.github.northmaxdev.coinplot.backend.core.web.request.CannotFormAPIRequestException;
+import io.github.northmaxdev.coinplot.lang.LocalDateInterval;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+
+import java.net.http.HttpClient;
+import java.util.Set;
+
+public abstract class AbstractExchangeRateFetchService<R extends ExchangeRateSetRequest, D>
+        extends AbstractRemoteResourceFetchService<R, D, Set<ExchangeRate>>
+        implements ExchangeRateService {
+
+    private final ExchangeRateRepository repository;
+
+    protected AbstractExchangeRateFetchService(
+            HttpClient httpClient,
+            ObjectMapper jsonParser,
+            ExchangeRateSetDTOMapper<D> dtoMapper,
+            ExchangeRateRepository repository) {
+        super(httpClient, jsonParser, dtoMapper);
+        this.repository = repository;
+    }
+
+    @Override
+    public final Set<ExchangeRate> getExchangeRates(
+            @Nullable Currency base,
+            Set<Currency> targets,
+            @Nonnull LocalDateInterval dateInterval) throws ResourceFetchFailureException {
+        try {
+            R apiRequest = createAPIRequest(base, targets, dateInterval);
+
+            // TODO: "Intelligent Caching" feature
+            if (apiRequest.isExplicitlyDefined()) {
+            } else {
+            }
+
+        } catch (CannotFormAPIRequestException e) {
+            throw new RemoteResourceFetchFailureException(e);
+        }
+    }
+
+    protected abstract @Nonnull R createAPIRequest(
+            @Nullable Currency base,
+            Set<Currency> targets,
+            @Nonnull LocalDateInterval dateInterval) throws CannotFormAPIRequestException;
+}
