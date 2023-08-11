033a8244a2e6f3c65cdaf48f92b0aa312ca4e4f3
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-11 10:34:35 +0300

    Use Period.ZERO as empty value in PeriodField

--- src/main/java/io/github/northmaxdev/coinplot/frontend/core/PeriodField.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/core/PeriodField.java
@@ -29,23 +29,33 @@ public final class PeriodField extends CustomField<Period> implements LocaleChan
     }
 
     @Override
+    public Period getEmptyValue() {
+        return Period.ZERO;
+    }
+
+    @Override
     protected Period generateModelValue() {
         int unitAmount = Ints.zeroIfNull(unitAmountField.getValue());
         return switch (unitSelect.getValue()) {
-            case null -> null;
             case DAYS -> Period.ofDays(unitAmount);
             case WEEKS -> Period.ofWeeks(unitAmount);
             case MONTHS -> Period.ofMonths(unitAmount);
             case YEARS -> Period.ofYears(unitAmount);
+            case null -> Period.ZERO;
             default -> throw new IllegalStateException("Unsupported ChronoUnit");
         };
     }
 
     @Override
     protected void setPresentationValue(Period period) {
-        // TODO:
-        //  Convert a given java.time.Period into a single-unit representation (truncating if
-        //  amount exceeds Integer.MAX_VALUE is OK so long as the user is notified about it).
-        //  Example: Period.of(2, 5, 12) --> Period.of(0, 0, 892)
+        if (period == null || period.isZero()) {
+            unitSelect.clear();
+            unitAmountField.clear();
+        } else {
+            // TODO:
+            //  Convert a given java.time.Period into a single-unit representation (truncating if
+            //  amount exceeds Integer.MAX_VALUE is OK so long as the user is notified about it).
+            //  Example: Period.of(2, 5, 12) --> Period.of(0, 0, 892)
+        }
     }
 }
