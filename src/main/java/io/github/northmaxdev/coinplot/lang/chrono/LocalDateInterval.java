924ff9712497f519ed0e0c203460e2c0fe0202f5
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-12 13:11:19 +0300

    Add java.time.Period stuff in LocalDateInterval

--- src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
@@ -5,8 +5,21 @@ package io.github.northmaxdev.coinplot.lang.chrono;
 import jakarta.annotation.Nonnull;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end) { // Both inclusive
+
+    public static LocalDateInterval ofStartPlusPeriod(@Nonnull LocalDate start, @Nonnull Period period) {
+        // FIXME: Take into consideration DateTimeException and ArithmeticException
+        LocalDate end = start.plus(period);
+        return new LocalDateInterval(start, end);
+    }
+
+    public Period toPeriod() {
+        // TODO: Cache this
+        // FIXME: LocalDateInterval end is inclusive, Period::between end is exclusive
+        return Period.between(start, end);
+    }
 
     public boolean isReversed() {
         return start.isAfter(end);
