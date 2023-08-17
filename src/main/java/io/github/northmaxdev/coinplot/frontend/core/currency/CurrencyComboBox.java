e68b365d583478e631e638b673b75bc5dc19b107
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-17 09:51:42 +0300

    Move currency combobox fetchItems impl to static util class

--- src/main/java/io/github/northmaxdev/coinplot/frontend/core/currency/CurrencyComboBox.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/core/currency/CurrencyComboBox.java
@@ -3,16 +3,12 @@
 package io.github.northmaxdev.coinplot.frontend.core.currency;
 
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
-import io.github.northmaxdev.coinplot.backend.core.ResourceFetchFailureException;
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.currency.CurrencyService;
 import io.github.northmaxdev.coinplot.frontend.i18n.I18NUtilities;
 import jakarta.annotation.Nonnull;
-
-import java.util.Set;
 
 public final class CurrencyComboBox extends ComboBox<Currency> implements LocaleChangeObserver {
 
@@ -30,19 +26,7 @@ public final class CurrencyComboBox extends ComboBox<Currency> implements Locale
     }
 
     public void fetchItems() {
-        boolean failed = false;
-        try {
-            Set<Currency> currencies = dataSource.getAvailableCurrencies();
-
-            ListDataProvider<Currency> dataProvider = new ListDataProvider<>(currencies);
-            dataProvider.setSortComparator(CurrencyComboBoxes.COMPARATOR);
-            setItems(dataProvider);
-        } catch (ResourceFetchFailureException e) {
-            failed = true;
-        } finally {
-            setEnabled(!failed);
-            setInvalid(failed);
-        }
+        CurrencyComboBoxes.fetchItemsInto(this, dataSource);
     }
 
     @Override
--- src/main/java/io/github/northmaxdev/coinplot/frontend/core/currency/CurrencyComboBoxes.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/core/currency/CurrencyComboBoxes.java
@@ -3,9 +3,13 @@
 package io.github.northmaxdev.coinplot.frontend.core.currency;
 
 import com.vaadin.flow.component.ItemLabelGenerator;
+import com.vaadin.flow.component.combobox.ComboBoxBase;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.function.SerializableComparator;
+import io.github.northmaxdev.coinplot.backend.core.ResourceFetchFailureException;
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
+import io.github.northmaxdev.coinplot.backend.core.currency.CurrencyService;
+import jakarta.annotation.Nonnull;
 
 import java.util.Comparator;
 import java.util.Set;
@@ -16,18 +20,33 @@ final class CurrencyComboBoxes { // Package-private
 
     public static final ItemLabelGenerator<Currency> CURRENCY_LABEL_GENERATOR = Currency::getName;
     public static final boolean ALLOW_CUSTOM_VALUE = false;
-    // See SerializableComparator class JavaDoc for more information
-    public static final SerializableComparator<Currency> COMPARATOR =
-            Comparator.comparing(Currency::getCode)::compare;
     public static final String FAILED_FETCH_ERROR_MESSAGE_KEY = "currency-combo-boxes.error-message.failed-fetch";
+
+    // See SerializableComparator class JavaDoc for more information
+    private static final SerializableComparator<Currency> COMPARATOR =
+            Comparator.comparing(Currency::getCode)::compare;
 
     private CurrencyComboBoxes() {
         throw new UnsupportedOperationException();
     }
 
-    public static ListDataProvider<Currency> createDataProvider(Set<Currency> currencies) {
-        ListDataProvider<Currency> dataProvider = new ListDataProvider<>(currencies);
-        dataProvider.setSortComparator(COMPARATOR);
-        return dataProvider;
+    public static <C extends ComboBoxBase<C, Currency, ?>> void fetchItemsInto(
+            @Nonnull C component,
+            @Nonnull CurrencyService dataSource) {
+        boolean failed = false;
+        try {
+            Set<Currency> currencies = dataSource.getAvailableCurrencies();
+
+            ListDataProvider<Currency> dataProvider = new ListDataProvider<>(currencies);
+            dataProvider.setSortComparator(COMPARATOR);
+            component.setItems(dataProvider);
+
+            // The flag is already "false"
+        } catch (ResourceFetchFailureException e) {
+            failed = true;
+        } finally {
+            component.setEnabled(!failed);
+            component.setInvalid(failed);
+        }
     }
 }
