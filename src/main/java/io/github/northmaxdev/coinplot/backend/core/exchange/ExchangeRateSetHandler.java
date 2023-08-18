4c84cc4ef9e1ce340e9f54df92d1a6ea431c9b22
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-18 13:20:26 +0300

    Add ExchangeRateSetHandler::handleIfNotNull

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetHandler.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetHandler.java
@@ -3,6 +3,7 @@
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
 import java.util.Set;
 
@@ -10,4 +11,11 @@ import java.util.Set;
 public interface ExchangeRateSetHandler {
 
     void handle(@Nonnull Set<ExchangeRate> dataset);
+
+    // Imitation of Kotlin/C# null-safe method calls
+    static void handleIfNotNull(@Nullable ExchangeRateSetHandler handler, @Nonnull Set<ExchangeRate> dataset) {
+        if (handler != null) {
+            handler.handle(dataset);
+        }
+    }
 }
