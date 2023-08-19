9cf7552c163cfe319f3b312800624039b1bc67fd
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-19 13:14:43 +0300

    Fix coding style issues for: package lang.chrono

--- src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
@@ -6,17 +6,25 @@ import jakarta.annotation.Nonnull;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Objects;
 
 public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end) { // Both inclusive
 
-    public static LocalDateInterval ofStartPlusPeriod(@Nonnull LocalDate start, @Nonnull Period period) {
+    public LocalDateInterval {
+        Objects.requireNonNull(start, "start is null");
+        Objects.requireNonNull(end, "end is null");
+    }
+
+    public static @Nonnull LocalDateInterval ofStartPlusPeriod(@Nonnull LocalDate start, @Nonnull Period period) {
+        Objects.requireNonNull(start, "start is null");
+        Objects.requireNonNull(period, "period is null");
+
         // FIXME: Take into consideration DateTimeException and ArithmeticException
         LocalDate end = start.plus(period);
         return new LocalDateInterval(start, end);
     }
 
-    public Period toPeriod() {
-        // TODO: Cache this
+    public @Nonnull Period toPeriod() {
         // FIXME: LocalDateInterval end is inclusive, Period::between end is exclusive
         return Period.between(start, end);
     }
@@ -25,7 +33,7 @@ public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end
         return start.isAfter(end);
     }
 
-    public LocalDateInterval reverse() {
+    public @Nonnull LocalDateInterval reverse() {
         if (start.isEqual(end)) {
             return this;
         }
