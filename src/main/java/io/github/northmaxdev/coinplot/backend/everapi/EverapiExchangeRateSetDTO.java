7b03ea92fbb04653b51ecb86d303435c0fe53e2b
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-20 16:44:16 +0300

    Implement EverapiExchangeRateSetDTO

--- src/main/java/io/github/northmaxdev/coinplot/backend/everapi/EverapiExchangeRateSetDTO.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/everapi/EverapiExchangeRateSetDTO.java
@@ -2,7 +2,19 @@
 
 package io.github.northmaxdev.coinplot.backend.everapi;
 
-public record EverapiExchangeRateSetDTO() {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-    // TODO: Implement this
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+// Keep in mind that this provider does not explicitly document the nullability of the provided data
+public record EverapiExchangeRateSetDTO(List<DataObject> data) {
+
+    @JsonIgnoreProperties({"datetime"})
+    public record DataObject(Map<String, ExchangeRateObject> currencies) {
+
+        @JsonIgnoreProperties({"code"})
+        public record ExchangeRateObject(BigDecimal value) {}
+    }
 }
