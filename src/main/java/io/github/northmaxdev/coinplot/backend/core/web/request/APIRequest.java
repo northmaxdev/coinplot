d76498a9caa24d14b73cab2efb213a599999558e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-21 18:48:00 +0300

    APIRequest: remove toHTTPRequestBuilder, remove default getHeaders impl and add nullability annotations

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/web/request/APIRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/web/request/APIRequest.java
@@ -5,25 +5,11 @@ package io.github.northmaxdev.coinplot.backend.core.web.request;
 import jakarta.annotation.Nonnull;
 
 import java.net.URI;
-import java.net.http.HttpRequest;
 import java.util.Map;
 
-// Semantically not a @FunctionalInterface (additional abstract methods may be added in the future)
 public interface APIRequest {
 
     @Nonnull URI getURI();
 
-    default Map<String, String> getHeaders() {
-        return Map.of();
-    }
-
-    default @Nonnull HttpRequest.Builder toHTTPRequestBuilder() {
-        URI uri = getURI();
-        Map<String, String> headers = getHeaders();
-
-        HttpRequest.Builder builder = HttpRequest.newBuilder(uri);
-        headers.forEach(builder::setHeader);
-
-        return builder;
-    }
+    @Nonnull Map<String, String> getHeaders();
 }
