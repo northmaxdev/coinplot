3d4aa4190d3c949f608120d69d3505a9b3891339
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-21 14:14:32 +0300

    Implement EverapiExchangeRateSetDTO

--- src/main/java/io/github/northmaxdev/coinplot/backend/everapi/EverapiExchangeRateSetDTO.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/everapi/EverapiExchangeRateSetDTO.java
@@ -5,14 +5,16 @@ package io.github.northmaxdev.coinplot.backend.everapi;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
-// Keep in mind that this provider does not explicitly document the nullability of the provided data
+// Reference: https://currencyapi.com/docs/range (last checked: 21-08-2023 14:12)
+// Keep in mind that this provider does not explicitly document the nullability of the provided data.
+// FIXME: It is unclear from the documentation above whether this DTO uses snake_case like the currency one
 public record EverapiExchangeRateSetDTO(List<DataObject> data) {
 
-    @JsonIgnoreProperties({"datetime"})
-    public record DataObject(Map<String, ExchangeRateObject> currencies) {
+    public record DataObject(LocalDateTime datetime, Map<String, ExchangeRateObject> currencies) {
 
         @JsonIgnoreProperties({"code"})
         public record ExchangeRateObject(BigDecimal value) {}
