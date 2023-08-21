01fed495609b7882758535c8ff0926032409fc56
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-22 01:19:46 +0300

    Implement FrankfurterConfiguration

--- src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/FrankfurterConfiguration.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/FrankfurterConfiguration.java
@@ -3,18 +3,30 @@
 package io.github.northmaxdev.coinplot.backend.frankfurter;
 
 import io.github.northmaxdev.coinplot.backend.core.web.request.CustomHostSupplier;
+import jakarta.annotation.Nullable;
 import org.apache.hc.core5.http.HttpHost;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URISyntaxException;
 import java.util.Optional;
 
 @Configuration
 public class FrankfurterConfiguration implements CustomHostSupplier {
 
-    // TODO: Implement this
+    @Value("${frankfurter.custom-host}")
+    private @Nullable String customHostLiteral = null;
 
     @Override
     public Optional<HttpHost> getCustomHost() {
-        return Optional.empty();
+        // FIXME: Come up with a better solution because this might be a potential bottleneck
+        return Optional.ofNullable(customHostLiteral)
+                .map(s -> {
+                    try {
+                        return HttpHost.create(s);
+                    } catch (URISyntaxException e) {
+                        return null;
+                    }
+                });
     }
 }
