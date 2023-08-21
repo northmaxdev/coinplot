52905a4794c9d112abcca818802b28b03fc400b8
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-21 22:26:25 +0300

    AbstractFrankfurterAPIRequest: allow nulls for customHost

--- src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/AbstractFrankfurterAPIRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/AbstractFrankfurterAPIRequest.java
@@ -4,6 +4,7 @@ package io.github.northmaxdev.coinplot.backend.frankfurter;
 
 import io.github.northmaxdev.coinplot.backend.core.web.request.AbstractAPIRequest;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import org.apache.hc.core5.http.HttpHost;
 
 import java.util.Objects;
@@ -17,11 +18,11 @@ public abstract class AbstractFrankfurterAPIRequest extends AbstractAPIRequest {
     private final @Nonnull HttpHost host;
 
     protected AbstractFrankfurterAPIRequest() {
-        this(PUBLIC_INSTANCE_HOST);
+        this(null);
     }
 
-    protected AbstractFrankfurterAPIRequest(@Nonnull HttpHost customHost) {
-        this.host = Objects.requireNonNull(customHost, "customHost is null");
+    protected AbstractFrankfurterAPIRequest(@Nullable HttpHost customHost) {
+        host = Objects.requireNonNullElse(customHost, PUBLIC_INSTANCE_HOST);
     }
 
     @Override
