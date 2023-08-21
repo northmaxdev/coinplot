7c19bdf7281c3e9f937b8c5b9cdb1bb8f88b2e91
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-21 16:08:21 +0300

    Implement FixerExchangeRateSetDTO

--- src/main/java/io/github/northmaxdev/coinplot/backend/fixer/FixerExchangeRateSetDTO.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/fixer/FixerExchangeRateSetDTO.java
@@ -3,10 +3,15 @@
 package io.github.northmaxdev.coinplot.backend.fixer;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Map;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record FixerExchangeRateSetDTO(Map<LocalDate, Map<String, BigDecimal>> rates) {}
+// Reference: https://fixer.io/documentation#timeseries (last checked: 21-08-2023 16:06)
+// Keep in mind that this provider does not explicitly document the nullability of the provided data.
+@JsonIgnoreProperties({"success", "timeseries", "start_date", "end_date"})
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record FixerExchangeRateSetDTO(String base, Map<LocalDate, Map<String, BigDecimal>> rates) {}
