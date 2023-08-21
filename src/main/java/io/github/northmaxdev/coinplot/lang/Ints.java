b01778a38076b32e494a22c6957f107ece140fe5
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-21 17:38:32 +0300

    Remove Ints::optionalOfNullable

--- src/main/java/io/github/northmaxdev/coinplot/lang/Ints.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/Ints.java
@@ -4,8 +4,6 @@ package io.github.northmaxdev.coinplot.lang;
 
 import jakarta.annotation.Nullable;
 
-import java.util.OptionalInt;
-
 public final class Ints {
 
     private Ints() {
@@ -14,9 +12,5 @@ public final class Ints {
 
     public static int zeroIfNull(@Nullable Integer i) {
         return i == null ? 0 : i;
-    }
-
-    public static OptionalInt optionalOfNullable(@Nullable Integer i) {
-        return i == null ? OptionalInt.empty() : OptionalInt.of(i);
     }
 }
