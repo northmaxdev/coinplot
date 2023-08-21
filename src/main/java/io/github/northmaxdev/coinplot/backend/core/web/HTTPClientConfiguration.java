5e5dd6d8fe4c852e29aa206b5fc379b7f98bceee
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-21 19:33:21 +0300

    Document the (undesired) possibility of a null HttpResponse::body

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/web/HTTPClientConfiguration.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/web/HTTPClientConfiguration.java
@@ -18,6 +18,10 @@ public class HTTPClientConfiguration {
     public HttpClient httpClient() {
         return HttpClient.newBuilder()
                 .connectTimeout(CONNECT_TIMEOUT)
+                // NOTE: The redirect thing is here only in the hopes that this will
+                // eliminate the chance of an HttpResponse::body returning null, which
+                // is NOT accounted for in AbstractRemoteDataFetchService::fetch
+                .followRedirects(HttpClient.Redirect.NEVER)
                 .version(Version.HTTP_2)
                 .build();
     }
