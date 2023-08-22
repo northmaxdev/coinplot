535e4608d712da286fa6099e4edf382bc2db9b56
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-22 13:09:10 +0300

    Implement FrankfurterExchangeRateSetDTOMapper

--- src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/FrankfurterExchangeRateSetDTOMapper.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/frankfurter/FrankfurterExchangeRateSetDTOMapper.java
@@ -2,14 +2,21 @@
 
 package io.github.northmaxdev.coinplot.backend.frankfurter;
 
+import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.currency.CurrencyService;
 import io.github.northmaxdev.coinplot.backend.core.exchange.AbstractExchangeRateSetDTOMapper;
 import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRate;
 import io.github.northmaxdev.coinplot.backend.core.web.response.DTOMappingException;
 import jakarta.annotation.Nonnull;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
 
 public final class FrankfurterExchangeRateSetDTOMapper extends AbstractExchangeRateSetDTOMapper<FrankfurterExchangeRateSetDTO> {
 
@@ -19,7 +26,24 @@ public final class FrankfurterExchangeRateSetDTOMapper extends AbstractExchangeR
 
     @Override
     public @Nonnull Set<ExchangeRate> map(@Nonnull FrankfurterExchangeRateSetDTO dto) throws DTOMappingException {
-        Objects.requireNonNull(dto, "dto is null");
-        throw new UnsupportedOperationException(); // TODO: Implement with unit tests
+        Objects.requireNonNull(dto);
+
+        Currency base = tryGetCurrency(dto.base());
+        return dto.rates()
+                .entrySet()
+                .stream()
+                .flatMap(entry -> flatten(entry.getValue(), base, entry.getKey()))
+                .collect(toSet());
+    }
+
+    // No need to annotate nullability for parameters of a private helper method
+    private Stream<ExchangeRate> flatten(Map<String, BigDecimal> m, Currency base, LocalDate date) throws DTOMappingException {
+        return m.entrySet()
+                .stream()
+                .map(entry -> {
+                    Currency target = tryGetCurrency(entry.getKey());
+                    BigDecimal value = entry.getValue();
+                    return new ExchangeRate(base, target, date, value);
+                });
     }
 }
---
+++ src/test/java/io/github/northmaxdev/coinplot/backend/frankfurter/FrankfurterExchangeRateSetDTOMapperTests.java
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.frankfurter;
+
+import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRate;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Map;
+import java.util.Set;
+
+import static io.github.northmaxdev.coinplot.TestUtilities.BAR_FRANC;
+import static io.github.northmaxdev.coinplot.TestUtilities.BAZ_POUND;
+import static io.github.northmaxdev.coinplot.TestUtilities.CURRENCY_SERVICE_MOCK;
+import static io.github.northmaxdev.coinplot.TestUtilities.FOO_DOLLAR;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FrankfurterExchangeRateSetDTOMapperTests {
+
+    @Test
+    void mapsDTO() {
+        // Case:
+        // Base = FOO
+        // Targets = BAR, BAZ
+        // Interval = [1st of January 2000, 2nd of January 2000]
+
+        var dto = new FrankfurterExchangeRateSetDTO("FOO", Map.of(
+                LocalDate.of(2000, Month.JANUARY, 1), Map.of(
+                        "BAR", BigDecimal.valueOf(1.567),
+                        "BAZ", BigDecimal.valueOf(2.301)
+                ),
+                LocalDate.of(2000, Month.JANUARY, 2), Map.of(
+                        "BAR", BigDecimal.valueOf(1.578),
+                        "BAZ", BigDecimal.valueOf(2.29)
+                )
+        ));
+        var mapper = new FrankfurterExchangeRateSetDTOMapper(CURRENCY_SERVICE_MOCK);
+
+        Set<ExchangeRate> expected = Set.of(
+                new ExchangeRate(FOO_DOLLAR, BAR_FRANC, LocalDate.of(2000, Month.JANUARY, 1), BigDecimal.valueOf(1.567)),
+                new ExchangeRate(FOO_DOLLAR, BAZ_POUND, LocalDate.of(2000, Month.JANUARY, 1), BigDecimal.valueOf(2.301)),
+                new ExchangeRate(FOO_DOLLAR, BAR_FRANC, LocalDate.of(2000, Month.JANUARY, 2), BigDecimal.valueOf(1.578)),
+                new ExchangeRate(FOO_DOLLAR, BAZ_POUND, LocalDate.of(2000, Month.JANUARY, 2), BigDecimal.valueOf(2.29))
+        );
+        Set<ExchangeRate> actual = mapper.map(dto);
+
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+}
