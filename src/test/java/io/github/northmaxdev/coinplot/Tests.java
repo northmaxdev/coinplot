73d0244870c5e1909faf15a5de0254c1b533709c
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-22 23:01:26 +0300

    Add more null-checks in test utility methods

--- src/test/java/io/github/northmaxdev/coinplot/Tests.java
+++ src/test/java/io/github/northmaxdev/coinplot/Tests.java
@@ -80,17 +80,19 @@ public final class Tests {
         assertThat(actual).containsExactlyInAnyOrderEntriesOf(expected);
     }
 
-    public static <R extends APIRequest> void verifyAPIRequestEquals(Class<R> requestClass) {
+    public static <R extends APIRequest> void verifyAPIRequestEquals(@Nonnull Class<R> requestClass) {
+        Objects.requireNonNull(requestClass);
         EqualsVerifier.forClass(requestClass)
-                // The class hierarchy of APIRequest implementations mixes between subclasses that add state and
-                // subclasses that merely add behavior. Running an EqualsVerifier with standard configuration leads to
-                // "subclass does not equal superclass" errors. Configuring withRedefinedSuperclass() leads to "subclass
-                // equals superclass but should not" errors. The combination of the former and latter is very weird
-                // considering that we're talking about the same equals/hashCode implementations being tested.
-                // Suppressing such strict inheritance checks passes the tests, but I'm not sure if this is actually
-                // appropriate for the issue at hand considering its documentation, so this may or may not lead to
-                // subtle bugs later on (hopefully not). In the meantime, the tested equals/hashCode implementations
-                // look completely OK.
+                // FIXME:
+                //  The class hierarchy of APIRequest implementations mixes between subclasses that add state and
+                //  subclasses that merely add behavior. Running an EqualsVerifier with standard configuration leads to
+                //  "subclass does not equal superclass" errors. Configuring withRedefinedSuperclass() leads to "subclass
+                //  equals superclass but should not" errors. The combination of the former and latter is very weird
+                //  considering that we're talking about the same equals/hashCode implementations being tested.
+                //  Suppressing such strict inheritance checks passes the tests, but I'm not sure if this is actually
+                //  appropriate for the issue at hand considering its documentation, so this may or may not lead to
+                //  subtle bugs later on (hopefully not). In the meantime, the tested equals/hashCode implementations
+                //  look completely OK.
                 .suppress(Warning.STRICT_INHERITANCE)
                 .verify();
     }
@@ -99,7 +101,8 @@ public final class Tests {
     // Persistence stuff //
     ///////////////////////
 
-    public static <T> void verifyJPAEntityEquals(Class<T> entityClass) {
+    public static <T> void verifyJPAEntityEquals(@Nonnull Class<T> entityClass) {
+        Objects.requireNonNull(entityClass);
         EqualsVerifier.forClass(entityClass)
                 .suppress(Warning.SURROGATE_KEY) // To only compare entity IDs for equality
                 .verify();
