57ff52b607b4a506af0ddab5045bcf88fd333216
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-24 22:41:40 +0300

    Use dummy ExchangeRate instances in ExchangeRateSetDTOMapper tests

--- src/test/java/io/github/northmaxdev/coinplot/backend/fixer/FixerExchangeRateSetDTOMapperTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/backend/fixer/FixerExchangeRateSetDTOMapperTests.java
@@ -11,38 +11,34 @@ import java.time.Month;
 import java.util.Map;
 import java.util.Set;
 
-import static io.github.northmaxdev.coinplot.Tests.BAR_FRANC;
-import static io.github.northmaxdev.coinplot.Tests.BAZ_POUND;
 import static io.github.northmaxdev.coinplot.Tests.CURRENCY_SERVICE_MOCK;
-import static io.github.northmaxdev.coinplot.Tests.FOO_DOLLAR;
+import static io.github.northmaxdev.coinplot.Tests.FOO_TO_BAR_JAN_1_2000;
+import static io.github.northmaxdev.coinplot.Tests.FOO_TO_BAR_JAN_2_2000;
+import static io.github.northmaxdev.coinplot.Tests.FOO_TO_BAZ_JAN_1_2000;
+import static io.github.northmaxdev.coinplot.Tests.FOO_TO_BAZ_JAN_2_2000;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FixerExchangeRateSetDTOMapperTests {
 
     @Test
     void mapsDTO() {
-        // Case:
-        // Base = FOO
-        // Targets = BAR, BAZ
-        // Interval = [1st of January 2000, 2nd of January 2000]
-
         var dto = new FixerExchangeRateSetDTO("FOO", Map.of(
                 LocalDate.of(2000, Month.JANUARY, 1), Map.of(
-                        "BAR", BigDecimal.valueOf(1.567),
-                        "BAZ", BigDecimal.valueOf(2.301)
+                        "BAR", BigDecimal.valueOf(1.618),
+                        "BAZ", BigDecimal.valueOf(3.1415)
                 ),
                 LocalDate.of(2000, Month.JANUARY, 2), Map.of(
-                        "BAR", BigDecimal.valueOf(1.578),
-                        "BAZ", BigDecimal.valueOf(2.29)
+                        "BAR", BigDecimal.valueOf(1.619),
+                        "BAZ", BigDecimal.valueOf(3.1401)
                 )
         ));
         var mapper = new FixerExchangeRateSetDTOMapper(CURRENCY_SERVICE_MOCK);
 
         Set<ExchangeRate> expected = Set.of(
-                new ExchangeRate(FOO_DOLLAR, BAR_FRANC, LocalDate.of(2000, Month.JANUARY, 1), BigDecimal.valueOf(1.567)),
-                new ExchangeRate(FOO_DOLLAR, BAZ_POUND, LocalDate.of(2000, Month.JANUARY, 1), BigDecimal.valueOf(2.301)),
-                new ExchangeRate(FOO_DOLLAR, BAR_FRANC, LocalDate.of(2000, Month.JANUARY, 2), BigDecimal.valueOf(1.578)),
-                new ExchangeRate(FOO_DOLLAR, BAZ_POUND, LocalDate.of(2000, Month.JANUARY, 2), BigDecimal.valueOf(2.29))
+                FOO_TO_BAR_JAN_1_2000,
+                FOO_TO_BAR_JAN_2_2000,
+                FOO_TO_BAZ_JAN_1_2000,
+                FOO_TO_BAZ_JAN_2_2000
         );
         Set<ExchangeRate> actual = mapper.map(dto);
 
