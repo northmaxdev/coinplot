be3c286b84678ba140a1ba0bd12ed552329114da
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-08-31 15:31:50 +0300

    Add ExchangeRateSetRequest::reduced

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetRequest.java
@@ -6,7 +6,9 @@ import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.web.request.APIRequest;
 import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface ExchangeRateSetRequest extends APIRequest {
@@ -17,4 +19,12 @@ public interface ExchangeRateSetRequest extends APIRequest {
     @Nonnull Set<Currency> getTargets();
 
     @Nonnull LocalDateInterval getDateInterval();
+
+    // Constructs a "reduced" version of this request by removing the provided exchanges.
+    // Calling this method with a null argument is permitted and is equivalent to an empty
+    // set, which in turn is effectively equivalent to removing no exchanges and should
+    // return this request as-is. If the reduction operation ends up removing all the
+    // exchanges contained in this request, then an empty Optional is returned.
+    // If a given exchange is not contained in this request, it is ignored.
+    Optional<? extends ExchangeRateSetRequest> reduced(@Nullable Set<Exchange> exchangesToRemove);
 }
