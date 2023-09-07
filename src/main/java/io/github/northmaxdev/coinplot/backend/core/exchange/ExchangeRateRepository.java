f1bdf84652c87876701e26236fb91d0fe64ac540
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-07 12:43:38 +0300

    Add ExchangeRateRepository::findMatching

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
@@ -2,13 +2,24 @@
 
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
+import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.db.EnhancedJPARepository;
+import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
+import jakarta.annotation.Nonnull;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface ExchangeRateRepository extends EnhancedJPARepository<ExchangeRate, Exchange> {
 
     // TODO:
-    //  JPQL implementations of at least some of EnhancedJPARepository
-    //  default methods (for performance reasons)
+    //  0. Unit tests
+    //  1. Implement as Java code
+    //  2. Profile
+    //  3. Implement as a JPQL query if needed
+    @Nonnull Set<ExchangeRate> findMatching(
+            @Nonnull Currency base,
+            @Nonnull Set<Currency> targets,
+            @Nonnull LocalDateInterval dateInterval);
 }
