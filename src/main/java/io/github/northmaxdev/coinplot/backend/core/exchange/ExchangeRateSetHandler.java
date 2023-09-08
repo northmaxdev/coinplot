9a911a4bf56e2dcf4cbd5c1a79d7e74a1d696824
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-08 15:08:40 +0300

    Remove previous implementation of a composite ExchangeRateSetHandler

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetHandler.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateSetHandler.java
@@ -3,29 +3,11 @@
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 
-import java.util.Objects;
 import java.util.Set;
 
 @FunctionalInterface
 public interface ExchangeRateSetHandler {
 
     void handle(@Nonnull Set<ExchangeRate> dataset);
-
-    // Imitation of Kotlin/C# null-safe method calls
-    static void handleIfNotNull(@Nullable ExchangeRateSetHandler handler, @Nonnull Set<ExchangeRate> dataset) {
-        Objects.requireNonNull(dataset);
-        if (handler != null) {
-            handler.handle(dataset);
-        }
-    }
-
-    static @Nonnull ExchangeRateSetHandler composedOf(@Nullable ExchangeRateSetHandler... handlers) {
-        return new SingleThreadedExchangeRateSetCompositeHandler(handlers);
-    }
-
-    static @Nonnull ExchangeRateSetHandler parallelComposedOf(@Nullable ExchangeRateSetHandler... handlers) {
-        return new ParallelExchangeRateSetCompositeHandler(handlers);
-    }
 }
