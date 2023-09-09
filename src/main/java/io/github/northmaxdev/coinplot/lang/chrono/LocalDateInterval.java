484081e75e915d8049f35f3382270f36b91f61c8
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-09 16:43:17 +0300

    LocalDateInterval: accept only start < end

--- src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
@@ -4,27 +4,32 @@ package io.github.northmaxdev.coinplot.lang.chrono;
 
 import jakarta.annotation.Nonnull;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-// Start inclusive, end exclusive
+// Endpoints: start is inclusive, end is exclusive
+// Mathematical notation: [start, end)
 public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end) {
 
     public LocalDateInterval {
         Objects.requireNonNull(start);
         Objects.requireNonNull(end);
+
+        if (!start.isBefore(end)) {
+            throw new IllegalArgumentException("start >= end");
+        }
     }
 
     public static @Nonnull LocalDateInterval calculate(@Nonnull LocalDate start, @Nonnull Period periodToAdd)
-            throws ArithmeticException {
+            throws DateTimeException, ArithmeticException {
         Objects.requireNonNull(start);
         Objects.requireNonNull(periodToAdd);
 
-        // Can LocalDate.plus(Period) throw a DateTimeException or not?
-        // The related JavaDocs do not seem to answer the question clearly.
         LocalDate end = start.plus(periodToAdd);
+        // No need to validate the period, the canonical constructor's bounds-check is enough
         return new LocalDateInterval(start, end);
     }
 
@@ -32,34 +37,7 @@ public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end
         return Period.between(start, end);
     }
 
-    public boolean isReversed() {
-        return start.isAfter(end);
-    }
-
-    public @Nonnull LocalDateInterval flipped() {
-        if (start.isEqual(end)) {
-            return this;
-        }
-
-        return new LocalDateInterval(end, start);
-    }
-
     public @Nonnull Stream<LocalDate> dates() {
-        if (start.isBefore(end)) {
-            return start.datesUntil(end);
-        }
-
-        // Given an interval of [2000-01-13, 2000-01-02), the resulting stream should include
-        // 13th of Jan and exclude 2nd of Jan, but simply calling end.datesUntil(start) produces
-        // a stream that is equivalent to the one produced if start < end. To solve this, we can
-        // simply shift both endpoints to the right by 1 day.
-        long daysToAdd = 1L;
-        LocalDate shiftedStart = start.plusDays(daysToAdd);
-        LocalDate shiftedEnd = end.plusDays(daysToAdd);
-        return shiftedEnd.datesUntil(shiftedStart);
-
-        // An alternative implementation:
-        //    return end.datesUntil(start)
-        //            .map(date -> date.plusDays(1L));
+        return start.datesUntil(end);
     }
 }
--- src/test/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateIntervalTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateIntervalTests.java
@@ -4,116 +4,33 @@ package io.github.northmaxdev.coinplot.lang.chrono;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.time.Period;
-import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class LocalDateIntervalTests {
 
     static final LocalDate JAN_1 = LocalDate.of(2000, Month.JANUARY, 1);
     static final LocalDate JAN_2 = LocalDate.of(2000, Month.JANUARY, 2);
-    static final LocalDate JAN_13 = LocalDate.of(2000, Month.JANUARY, 13);
-    static final LocalDate JAN_14 = LocalDate.of(2000, Month.JANUARY, 14);
 
-    @ParameterizedTest
-    @MethodSource("provideIsReversedExpectations")
-    @DisplayName("isReversed returns the expected value for a given interval")
-    void isReversedWhenExpected(LocalDate start, LocalDate end, boolean expected) {
-        LocalDateInterval interval = new LocalDateInterval(start, end);
-
-        assertThat(interval.isReversed()).isEqualTo(expected);
-    }
-
-    Stream<Arguments> provideIsReversedExpectations() {
-        return Stream.of(
-                arguments(JAN_1, JAN_1, false),
-                arguments(JAN_1, JAN_2, false),
-                arguments(JAN_1, JAN_14, false),
-                arguments(JAN_14, JAN_1, true),
-                arguments(JAN_14, JAN_13, true)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("providePeriods")
-    @DisplayName("toPeriod returns the expected value for a given interval")
-    void expectedPeriodEqualsActual(LocalDate start, LocalDate end, Period expected) {
-        LocalDateInterval interval = new LocalDateInterval(start, end);
-
-        assertThat(interval.toPeriod()).isEqualTo(expected);
-    }
-
-    Stream<Arguments> providePeriods() {
-        return Stream.of(
-                arguments(JAN_1, JAN_1, Period.ZERO),
-                arguments(JAN_1, JAN_2, Period.ofDays(1)),
-                arguments(JAN_1, JAN_14, Period.ofDays(13)),
-                arguments(JAN_14, JAN_1, Period.ofDays(-13)),
-                arguments(JAN_14, JAN_13, Period.ofDays(-1))
-        );
+    @Test
+    @DisplayName("Canonical constructor throws IAE if start > end")
+    void constructorRejectsDescendingDates() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new LocalDateInterval(JAN_2, JAN_1));
     }
 
     @Test
-    @DisplayName("Method \"dates\" returns expected contents on ascending interval")
-    void datesAscending() {
-        LocalDateInterval interval = new LocalDateInterval(JAN_2, JAN_13);
-
-        Stream<LocalDate> expected = Stream.of(
-                LocalDate.of(2000, Month.JANUARY, 2),
-                LocalDate.of(2000, Month.JANUARY, 3),
-                LocalDate.of(2000, Month.JANUARY, 4),
-                LocalDate.of(2000, Month.JANUARY, 5),
-                LocalDate.of(2000, Month.JANUARY, 6),
-                LocalDate.of(2000, Month.JANUARY, 7),
-                LocalDate.of(2000, Month.JANUARY, 8),
-                LocalDate.of(2000, Month.JANUARY, 9),
-                LocalDate.of(2000, Month.JANUARY, 10),
-                LocalDate.of(2000, Month.JANUARY, 11),
-                LocalDate.of(2000, Month.JANUARY, 12)
-                // Interval is end-exclusive, no 13
-        );
-        Stream<LocalDate> actual = interval.dates();
-
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected.toList());
+    @DisplayName("Canonical constructor throws IAE if start == end")
+    void constructorRejectsEqualDates() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new LocalDateInterval(JAN_2, JAN_2));
     }
 
     @Test
-    @DisplayName("Method \"dates\" returns expected contents on descending interval")
-    void datesDescending() {
-        LocalDateInterval interval = new LocalDateInterval(JAN_13, JAN_2);
-
-        Stream<LocalDate> expected = Stream.of(
-                LocalDate.of(2000, Month.JANUARY, 13),
-                LocalDate.of(2000, Month.JANUARY, 12),
-                LocalDate.of(2000, Month.JANUARY, 11),
-                LocalDate.of(2000, Month.JANUARY, 10),
-                LocalDate.of(2000, Month.JANUARY, 9),
-                LocalDate.of(2000, Month.JANUARY, 8),
-                LocalDate.of(2000, Month.JANUARY, 7),
-                LocalDate.of(2000, Month.JANUARY, 6),
-                LocalDate.of(2000, Month.JANUARY, 5),
-                LocalDate.of(2000, Month.JANUARY, 4),
-                LocalDate.of(2000, Month.JANUARY, 3)
-                // Interval is end-exclusive, no 2
-        );
-        Stream<LocalDate> actual = interval.dates();
-
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected.toList());
-    }
-
-    @Test
-    @DisplayName("Method \"dates\" returns an empty stream on equal start/end")
-    void datesEmpty() {
-        LocalDateInterval interval = new LocalDateInterval(JAN_13, JAN_13);
-
-        assertThat(interval.dates()).isEmpty();
+    @DisplayName("Canonical constructor does not throw if start < end")
+    void constructorAcceptsAscendingDates() {
+        assertThatNoException().isThrownBy(() -> new LocalDateInterval(JAN_1, JAN_2));
     }
 }
