a1c1666d13e0744ea51e4e45100d6201fc9756c3
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-09 17:35:55 +0300

    Add ExchangeBatch

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeBatch.java
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.exchange;
+
+import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
+import io.github.northmaxdev.coinplot.lang.MoreCollections;
+import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
+import jakarta.annotation.Nonnull;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
+
+// A "compressed" way to represent a bunch of exchanges. The following Exchange instances:
+//
+//     {FOO, BAR, 2000-01-01}
+//     {FOO, BAZ, 2000-01-01}
+//     {FOO, BAR, 2000-01-02}
+//     {FOO, BAZ, 2000-01-02}
+//
+// ...can be represented as the following ExchangeBatch:
+//
+//     {base=FOO, targets={BAR,BAZ}, dateInterval=[2000-01-01, 2000-01-03)}
+//
+// An ExchangeBatch can be very easily converted to a collection of Exchange instances, but not the other way around.
+// This is a general-purpose concept that is used by different parts of the codebase, such as the DB or web layers.
+public record ExchangeBatch(@Nonnull Currency base, @Nonnull Set<Currency> targets, @Nonnull LocalDateInterval dateInterval) {
+
+    public ExchangeBatch {
+        Objects.requireNonNull(base);
+        MoreCollections.requireNonEmptyWithoutNulls(targets);
+        Objects.requireNonNull(dateInterval);
+    }
+
+    public @Nonnull Stream<Exchange> toExchanges() {
+        return dateInterval.dates()
+                .mapMulti((date, buffer) -> {
+                    for (Currency target : targets) {
+                        Exchange exchange = new Exchange(base, target, date);
+                        buffer.accept(exchange);
+                    }
+                });
+    }
+}
---
+++ src/test/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeBatchTests.java
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.exchange;
+
+import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static io.github.northmaxdev.coinplot.Tests.BAR_FRANC;
+import static io.github.northmaxdev.coinplot.Tests.BAZ_POUND;
+import static io.github.northmaxdev.coinplot.Tests.FOO_DOLLAR;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExchangeBatchTests {
+
+    @Test
+    @DisplayName("toExchanges produces expected contents")
+    void toExchanges() {
+        ExchangeBatch batch = new ExchangeBatch(
+                FOO_DOLLAR,
+                Set.of(BAR_FRANC, BAZ_POUND),
+                new LocalDateInterval(
+                        LocalDate.of(2000, Month.JANUARY, 1),
+                        LocalDate.of(2000, Month.JANUARY, 3)
+                )
+        );
+
+        Stream<Exchange> expected = Stream.of(
+                new Exchange(FOO_DOLLAR, BAR_FRANC, LocalDate.of(2000, Month.JANUARY, 1)),
+                new Exchange(FOO_DOLLAR, BAZ_POUND, LocalDate.of(2000, Month.JANUARY, 1)),
+                new Exchange(FOO_DOLLAR, BAR_FRANC, LocalDate.of(2000, Month.JANUARY, 2)),
+                new Exchange(FOO_DOLLAR, BAZ_POUND, LocalDate.of(2000, Month.JANUARY, 2))
+        );
+        Stream<Exchange> actual = batch.toExchanges();
+
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected.toList());
+    }
+}
