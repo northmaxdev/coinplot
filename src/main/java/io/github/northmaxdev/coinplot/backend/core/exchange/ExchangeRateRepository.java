37196e5a2bb08d99bb06616048200327819fc1af
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-15 17:52:03 +0300

    Update ExchangeRateRepository

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
@@ -2,24 +2,22 @@
 
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
-import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
-import io.github.northmaxdev.coinplot.backend.core.db.EnhancedJPARepository;
-import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
+import io.github.northmaxdev.coinplot.backend.core.db.SetCrudRepository;
 import jakarta.annotation.Nonnull;
 import org.springframework.stereotype.Repository;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Repository
-public interface ExchangeRateRepository extends EnhancedJPARepository<ExchangeRate, Exchange> {
+public interface ExchangeRateRepository extends SetCrudRepository<ExchangeRate, Exchange> {
 
     // TODO:
-    //  0. Unit tests
-    //  1. Implement as Java code
-    //  2. Profile
-    //  3. Implement as a JPQL query if needed
-    @Nonnull Set<ExchangeRate> findMatching(
-            @Nonnull Currency base,
-            @Nonnull Set<Currency> targets,
-            @Nonnull LocalDateInterval dateInterval);
+    //  Consider overriding findAllById(Iterable<Exchange>) with a JPQL query,
+    //  as it is a frequently-accessed method.
+
+    default @Nonnull Set<ExchangeRate> findAllById(@Nonnull ExchangeBatch batch) {
+        Objects.requireNonNull(batch);
+        return findAllById(batch.toSet());
+    }
 }
