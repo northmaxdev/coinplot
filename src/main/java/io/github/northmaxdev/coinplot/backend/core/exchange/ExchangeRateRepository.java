dbb38261f73a42a13527184d1e705f6b5592778c
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-16 12:53:26 +0300

    Update ExchangeRateRepository

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeRateRepository.java
@@ -2,15 +2,33 @@
 
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
+import io.github.northmaxdev.coinplot.backend.core.db.EntityExistenceQueryableRepository;
 import io.github.northmaxdev.coinplot.backend.core.db.SetCrudRepository;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Objects;
 import java.util.Set;
 
 @Repository
-public interface ExchangeRateRepository extends SetCrudRepository<ExchangeRate, Exchange> {
+public interface ExchangeRateRepository extends
+        SetCrudRepository<ExchangeRate, Exchange>,
+        EntityExistenceQueryableRepository<ExchangeRate, Exchange> {
+
+    // TODO:
+    //  Consider overriding existAllById(Iterable<Exchange>) with a JPQL query,
+    //  as it is a frequently-accessed method.
+
+    default boolean existAllById(@Nonnull ExchangeBatch batch) {
+        Objects.requireNonNull(batch);
+        @Nullable Boolean result = existAllById(batch.toSet());
+        if (result == null) {
+            // Since an ExchangeBatch cannot be empty, this should never happen
+            throw new IllegalStateException("Bug: existAllById(ExchangeBatch.toSet()) returned null");
+        }
+        return result;
+    }
 
     // TODO:
     //  Consider overriding findAllById(Iterable<Exchange>) with a JPQL query,
