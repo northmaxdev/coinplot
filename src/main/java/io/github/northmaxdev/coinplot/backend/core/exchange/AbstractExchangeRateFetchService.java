fe146f76a55af8de90405b788ee056f44bcbc65a
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-16 13:21:52 +0300

    Finish implementation of AbstractExchangeRateFetchService

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/AbstractExchangeRateFetchService.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/AbstractExchangeRateFetchService.java
@@ -4,11 +4,9 @@ package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.northmaxdev.coinplot.backend.core.FailedDataFetchException;
-import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.web.AbstractRemoteDataFetchService;
 import io.github.northmaxdev.coinplot.backend.core.web.FailedRemoteDataFetchException;
 import io.github.northmaxdev.coinplot.backend.core.web.request.CannotCreateAPIRequestException;
-import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
 import jakarta.annotation.Nonnull;
 
 import java.net.http.HttpClient;
@@ -31,33 +29,27 @@ public abstract class AbstractExchangeRateFetchService<R extends ExchangeRateSet
     }
 
     @Override
-    public final @Nonnull Set<ExchangeRate> getExchangeRates(
-            @Nonnull Currency base,
-            @Nonnull Set<Currency> targets,
-            @Nonnull LocalDateInterval dateInterval) throws FailedDataFetchException {
+    public final @Nonnull Set<ExchangeRate> getExchangeRates(@Nonnull ExchangeBatch exchanges) throws FailedDataFetchException {
+        // Cases:
+        //     The desired data is FULLY available in the repo -> no requests sent, load from repo directly
+        //     The desired data is NOT available in the repo -> send a request, populate repo
+        //     The desired data is PARTIALLY available in the repo -> send a request as if the data is not available at all, populate repo
+        // Needless to say, asking for data that is either fully present or fully absent is handled efficiently,
+        // while asking for partially present data should lead to operational redundancies (and thus, performance hits).
+
+        // 'exchanges' is implicitly null-checked
+        if (repository.existAllById(exchanges)) {
+            return repository.findAllById(exchanges);
+        }
+
         try {
-            // The general idea:
-            // [1] Fetch from the repository first. There are three options for what we could get:
-            //     [1.1] The returned set contains ALL the stuff we requested
-            //     [1.2] The returned set contains a part of the stuff we requested
-            //     [1.3] The returned set contains none of the stuff we requested
-            // [2] Create an ExchangeRateSetRequest from our query, then immediately reduce it by
-            //     the stuff we fetched from repo:
-            //     [2.1] Case [1.1] --> the created request gets reduced to nothing, which means we
-            //           have all the required data in the repo, therefore, return without sending
-            //           any API requests.
-            //     [2.2] Case [1.2] --> the created request only contains the stuff we are missing,
-            //           therefore, send the API request, fill the repo and re-fetch again
-            //     [2.3] Case [1.3] --> the created request contains the entire query, therefore,
-            //           send the API request, fill the repo and re-fetch again
-            throw new UnsupportedOperationException();
+            R request = createAPIRequest(exchanges);
+            Set<ExchangeRate> exchangeRates = fetch(request);
+            return repository.saveAll(exchangeRates);
         } catch (CannotCreateAPIRequestException e) {
             throw new FailedRemoteDataFetchException(e);
         }
     }
 
-    protected abstract @Nonnull R createAPIRequest(
-            @Nonnull Currency base,
-            @Nonnull Set<Currency> targets,
-            @Nonnull LocalDateInterval dateInterval) throws CannotCreateAPIRequestException;
+    protected abstract @Nonnull R createAPIRequest(@Nonnull ExchangeBatch exchanges) throws CannotCreateAPIRequestException;
 }
