c62b106b759c8ea8bece61b87349d026b153324c
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-17 18:26:17 +0300

    Use Currency::createSet in FixerCurrencySetDTOMapper

--- src/main/java/io/github/northmaxdev/coinplot/backend/fixer/FixerCurrencySetDTOMapper.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/fixer/FixerCurrencySetDTOMapper.java
@@ -4,23 +4,16 @@ package io.github.northmaxdev.coinplot.backend.fixer;
 
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.currency.CurrencySetDTOMapper;
-import io.github.northmaxdev.coinplot.backend.core.web.response.DTOMappingException;
 import jakarta.annotation.Nonnull;
 
 import java.util.Objects;
 import java.util.Set;
 
-import static java.util.stream.Collectors.toSet;
-
 public final class FixerCurrencySetDTOMapper implements CurrencySetDTOMapper<FixerCurrencySetDTO> {
 
     @Override
-    public @Nonnull Set<Currency> map(@Nonnull FixerCurrencySetDTO dto) throws DTOMappingException {
-        return Objects.requireNonNull(dto)
-                .symbols()
-                .entrySet()
-                .stream()
-                .map(Currency::ofMapEntry)
-                .collect(toSet());
+    public @Nonnull Set<Currency> map(@Nonnull FixerCurrencySetDTO dto) {
+        Objects.requireNonNull(dto);
+        return Currency.createSet(dto.symbols());
     }
 }
