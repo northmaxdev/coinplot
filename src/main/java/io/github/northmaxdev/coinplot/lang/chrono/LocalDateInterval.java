7b82f268dc15569047ddc5511cd975260e6ee28b
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-17 12:49:54 +0300

    Override LocalDateInterval::toString

--- src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
@@ -7,6 +7,7 @@ import jakarta.annotation.Nonnull;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -39,5 +40,11 @@ public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end
 
     public @Nonnull Stream<LocalDate> dates() {
         return start.datesUntil(end);
+    }
+
+    @Override
+    public String toString() {
+        // TODO: This can be lazily-evaluated and cached
+        return '[' + start.format(DateTimeFormatter.ISO_LOCAL_DATE) + ", " + end.format(DateTimeFormatter.ISO_LOCAL_DATE) + ')';
     }
 }
