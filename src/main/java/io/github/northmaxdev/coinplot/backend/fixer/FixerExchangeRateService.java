5d0513f800ed346a7554a15e7479d1006d6618b2
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-17 12:01:30 +0300

    Refactor FixerExchangeRateService

--- src/main/java/io/github/northmaxdev/coinplot/backend/fixer/FixerExchangeRateService.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/fixer/FixerExchangeRateService.java
@@ -7,11 +7,11 @@ import io.github.northmaxdev.coinplot.backend.core.exchange.AbstractExchangeRate
 import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeBatch;
 import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRateRepository;
 import io.github.northmaxdev.coinplot.backend.core.web.request.CannotCreateAPIRequestException;
+import io.github.northmaxdev.coinplot.backend.core.web.response.JSONMapper;
 import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.net.http.HttpClient;
 import java.util.Objects;
 
@@ -28,21 +28,22 @@ public final class FixerExchangeRateService extends
             @Nonnull FixerCurrencyService currencyService,
             @Nonnull ExchangeRateRepository repository,
             @Nonnull FixerConfiguration config) {
-        super(httpClient, jsonParser, new FixerExchangeRateSetDTOMapper(currencyService), repository);
+        super(
+                httpClient,
+                jsonParser,
+                JSONMapper.forClass(FixerExchangeRateSetDTO.class),
+                new FixerExchangeRateSetDTOMapper(currencyService),
+                repository
+        );
         this.config = Objects.requireNonNull(config);
     }
 
     @Override
-    protected @Nonnull FixerExchangeRateSetRequest createAPIRequest(@Nonnull ExchangeBatch exchanges) throws CannotCreateAPIRequestException {
+    protected @Nonnull FixerExchangeRateSetRequest createAPIRequest(@Nonnull ExchangeBatch exchanges)
+            throws CannotCreateAPIRequestException {
+        // No need to null-check 'exchanges'
         return config.getAccessKey()
                 .map(accessKeyValue -> new FixerExchangeRateSetRequest(accessKeyValue, exchanges))
                 .orElseThrow(CannotCreateAPIRequestException::forNoAccessKey);
-    }
-
-    @Override
-    protected @Nonnull FixerExchangeRateSetDTO parseResponseBody(
-            @Nonnull byte[] responseBody,
-            @Nonnull ObjectMapper jsonParser) throws IOException {
-        return jsonParser.readValue(responseBody, FixerExchangeRateSetDTO.class);
     }
 }
