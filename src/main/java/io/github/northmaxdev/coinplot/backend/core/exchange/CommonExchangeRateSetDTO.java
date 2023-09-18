5b0bc397fe7f0e46c662edcf05054eb148207569
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-18 16:43:23 +0300

    Add CommonExchangeRateSetDTO

---
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/CommonExchangeRateSetDTO.java
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.backend.core.exchange;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.annotation.Nonnull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+
+// Common DTO for many web APIs.
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record CommonExchangeRateSetDTO(@Nonnull String base, @Nonnull Map<LocalDate, Map<String, BigDecimal>> rates) {}
