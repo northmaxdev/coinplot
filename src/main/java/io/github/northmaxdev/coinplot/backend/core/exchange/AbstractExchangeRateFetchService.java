4d7bdab3d5c67d597d69b0228689fe68a1239dba
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-18 17:52:49 +0300

    Update AbstractExchangeRateFetchService

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/AbstractExchangeRateFetchService.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/AbstractExchangeRateFetchService.java
@@ -3,10 +3,9 @@
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.northmaxdev.coinplot.backend.core.FailedDataFetchException;
-import io.github.northmaxdev.coinplot.backend.core.web.AbstractRemoteDataFetchService;
+import io.github.northmaxdev.coinplot.backend.core.web.AbstractRemoteDatasetService;
 import io.github.northmaxdev.coinplot.backend.core.web.request.CannotCreateAPIRequestException;
-import io.github.northmaxdev.coinplot.backend.core.web.response.JSONMapper;
+import io.github.northmaxdev.coinplot.backend.core.web.response.JSONParsingStrategy;
 import jakarta.annotation.Nonnull;
 import org.slf4j.Logger;
 
@@ -15,7 +14,7 @@ import java.util.Objects;
 import java.util.Set;
 
 public abstract class AbstractExchangeRateFetchService<R extends ExchangeRateSetRequest, D>
-        extends AbstractRemoteDataFetchService<R, D, Set<ExchangeRate>>
+        extends AbstractRemoteDatasetService<ExchangeRate, R, D>
         implements ExchangeRateService {
 
     private final @Nonnull ExchangeRateRepository repository;
@@ -23,21 +22,21 @@ public abstract class AbstractExchangeRateFetchService<R extends ExchangeRateSet
     protected AbstractExchangeRateFetchService(
             @Nonnull HttpClient httpClient,
             @Nonnull ObjectMapper jsonParser,
-            @Nonnull JSONMapper<D> jsonMapper,
+            @Nonnull JSONParsingStrategy<D> jsonParsingStrategy,
             @Nonnull ExchangeRateSetDTOMapper<D> dtoMapper,
             @Nonnull ExchangeRateRepository repository) {
-        super(httpClient, jsonParser, jsonMapper, dtoMapper);
+        super(httpClient, jsonParser, jsonParsingStrategy, dtoMapper);
         this.repository = Objects.requireNonNull(repository);
     }
 
     @Override
-    public final @Nonnull Set<ExchangeRate> getExchangeRates(@Nonnull ExchangeBatch exchanges) throws FailedDataFetchException {
+    public final @Nonnull Set<ExchangeRate> getAvailableExchangeRates(@Nonnull ExchangeBatch exchanges) {
         // Cases:
         //     The desired data is FULLY available in the repo -> no requests sent, load from repo directly
         //     The desired data is NOT available in the repo -> send a request, populate repo
         //     The desired data is PARTIALLY available in the repo -> send a request as if the data is not available at all, populate repo
         // Needless to say, asking for data that is either fully present or fully absent is handled efficiently,
-        // while asking for partially present data should lead to operational redundancies (and thus, performance hits).
+        // while asking for partially present data should lead to operational redundancies (and thus, potential performance hits).
         Objects.requireNonNull(exchanges);
         Logger logger = getLogger();
 
