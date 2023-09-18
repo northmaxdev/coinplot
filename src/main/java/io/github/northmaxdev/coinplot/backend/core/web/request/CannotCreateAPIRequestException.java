15eac49783a97f72b6982c33a3827ab2db2c854d
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-18 17:53:55 +0300

    Seal CannotCreateAPIRequestException and inline 'no access key' message

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/web/request/CannotCreateAPIRequestException.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/web/request/CannotCreateAPIRequestException.java
@@ -5,15 +5,13 @@ package io.github.northmaxdev.coinplot.backend.core.web.request;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
-public class CannotCreateAPIRequestException extends Exception { // Non-final
-
-    private static final String NO_ACCESS_KEY_MESSAGE = "No access key";
+public final class CannotCreateAPIRequestException extends Exception {
 
     public CannotCreateAPIRequestException(@Nullable String message) {
         super(message);
     }
 
     public static @Nonnull CannotCreateAPIRequestException forNoAccessKey() {
-        return new CannotCreateAPIRequestException(NO_ACCESS_KEY_MESSAGE);
+        return new CannotCreateAPIRequestException("No access key");
     }
 }
