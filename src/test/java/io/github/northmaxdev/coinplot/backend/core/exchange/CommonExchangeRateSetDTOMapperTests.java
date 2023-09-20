0009f85fa857a914bac21b58e0dc27f952fcdb08
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-20 10:24:10 +0300

    CommonExchangeRateSetDTOMapper: add additional unit tests

--- src/test/java/io/github/northmaxdev/coinplot/backend/core/exchange/CommonExchangeRateSetDTOMapperTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/backend/core/exchange/CommonExchangeRateSetDTOMapperTests.java
@@ -2,13 +2,13 @@
 
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Map;
-import java.util.Set;
 
 import static io.github.northmaxdev.coinplot.Tests.CURRENCY_SERVICE_MOCK;
 import static io.github.northmaxdev.coinplot.Tests.FOO_TO_BAR_JAN_1_2000;
@@ -33,14 +33,47 @@ class CommonExchangeRateSetDTOMapperTests {
         ));
         var mapper = new CommonExchangeRateSetDTOMapper(CURRENCY_SERVICE_MOCK);
 
-        Set<ExchangeRate> expected = Set.of(
+        assertThat(mapper.map(dto)).containsExactlyInAnyOrder(
                 FOO_TO_BAR_JAN_1_2000,
                 FOO_TO_BAR_JAN_2_2000,
                 FOO_TO_BAZ_JAN_1_2000,
                 FOO_TO_BAZ_JAN_2_2000
         );
-        Set<ExchangeRate> actual = mapper.map(dto);
+    }
 
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    @Test
+    @DisplayName("Returns an empty Set on unknown base currency code")
+    void emptySetOnUnknownBase() {
+        var dto = new CommonExchangeRateSetDTO("XTS", Map.of(
+                LocalDate.of(2000, Month.JANUARY, 1), Map.of(
+                        "BAR", BigDecimal.valueOf(1.618),
+                        "BAZ", BigDecimal.valueOf(3.1415)
+                ),
+                LocalDate.of(2000, Month.JANUARY, 2), Map.of(
+                        "BAR", BigDecimal.valueOf(1.619),
+                        "BAZ", BigDecimal.valueOf(3.1401)
+                )
+        ));
+        var mapper = new CommonExchangeRateSetDTOMapper(CURRENCY_SERVICE_MOCK);
+
+        assertThat(mapper.map(dto)).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Skips unknown target currency codes")
+    void skipsUnknownTargets() {
+        var dto = new CommonExchangeRateSetDTO("FOO", Map.of(
+                LocalDate.of(2000, Month.JANUARY, 1), Map.of(
+                        "BAR", BigDecimal.valueOf(1.618),
+                        "EUR", BigDecimal.valueOf(16.3264)
+                ),
+                LocalDate.of(2000, Month.JANUARY, 2), Map.of(
+                        "BAR", BigDecimal.valueOf(1.619),
+                        "EUR", BigDecimal.valueOf(16.6432)
+                )
+        ));
+        var mapper = new CommonExchangeRateSetDTOMapper(CURRENCY_SERVICE_MOCK);
+
+        assertThat(mapper.map(dto)).containsExactlyInAnyOrder(FOO_TO_BAR_JAN_1_2000, FOO_TO_BAR_JAN_2_2000);
     }
 }
