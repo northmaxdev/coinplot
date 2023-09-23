924b7011e7fe96d52821e7a1c8da47b6c1d73f09
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-23 23:58:09 +0300

    ExchangeBatch: streamline constructor validation using built-in stuff

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeBatch.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/ExchangeBatch.java
@@ -3,7 +3,6 @@
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
-import io.github.northmaxdev.coinplot.lang.MoreCollections;
 import io.github.northmaxdev.coinplot.lang.chrono.LocalDateInterval;
 import jakarta.annotation.Nonnull;
 
@@ -25,19 +24,21 @@ import static java.util.stream.Collectors.toUnmodifiableSet;
 //     {base=FOO, targets={BAR,BAZ}, dateInterval=[2000-01-01, 2000-01-03)}
 //
 // An ExchangeBatch can be very easily converted to a sequence of Exchange instances, but not the other way around.
-// This is a general-purpose concept that is used by different parts of the codebase, such as the DB or web layers.
-//
 // The primary invariant of ExchangeBatch is that it cannot be empty: that is, 'targets' always contains at least one
 // non-null currency and 'dateInterval' always contains at least one date. Thus, methods like stream() and toSet()
 // likewise never return empty values.
+//
+// This is a general-purpose domain concept.
 public record ExchangeBatch(@Nonnull Currency base, @Nonnull Set<Currency> targets, @Nonnull LocalDateInterval dateInterval) {
 
     public ExchangeBatch(@Nonnull Currency base, @Nonnull Set<Currency> targets, @Nonnull LocalDateInterval dateInterval) {
         this.base = Objects.requireNonNull(base);
         this.dateInterval = Objects.requireNonNull(dateInterval);
 
-        MoreCollections.requireNonEmptyWithoutNulls(targets);
-        this.targets = Set.copyOf(targets);
+        if (targets.isEmpty()) { // Implicit null-check on the set itself
+            throw new IllegalArgumentException("An ExchangeBatch must contain at least one target");
+        }
+        this.targets = Set.copyOf(targets); // Implicit null-checks on the set's items
     }
 
     public @Nonnull Stream<Exchange> stream() {
