844c1e697ece380c93a8f028aa55f2045e8bcddc
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-24 16:42:17 +0300

    StrictComponents: add overload for Collection

--- src/main/java/io/github/northmaxdev/coinplot/frontend/common/StrictComponents.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/common/StrictComponents.java
@@ -4,6 +4,8 @@ package io.github.northmaxdev.coinplot.frontend.common;
 
 import jakarta.annotation.Nonnull;
 
+import java.util.Collection;
+
 // Utilities for the implementation of Strict* components.
 final class StrictComponents { // Package-private
 
@@ -13,6 +15,12 @@ final class StrictComponents { // Package-private
 
     static void checkOptionCount(int minCount, @Nonnull Object[] options) {
         if (options.length < minCount) { // Implicit null-check on the array
+            throw new IllegalArgumentException("Must supply at least " + minCount + " options");
+        }
+    }
+
+    static void checkOptionCount(int minCount, @Nonnull Collection<?> options) {
+        if (options.size() < minCount) { // Implicit null-check on the collection
             throw new IllegalArgumentException("Must supply at least " + minCount + " options");
         }
     }
