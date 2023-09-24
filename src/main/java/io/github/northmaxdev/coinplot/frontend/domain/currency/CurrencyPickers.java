7c1ca9865b90cc3d783c61eb7d6d3cb80f799b64
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-24 15:40:03 +0300

    CurrencyPickers: use ListDataProviders in loadItems

--- src/main/java/io/github/northmaxdev/coinplot/frontend/domain/currency/CurrencyPickers.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/currency/CurrencyPickers.java
@@ -4,9 +4,9 @@ package io.github.northmaxdev.coinplot.frontend.domain.currency;
 
 import com.vaadin.flow.component.combobox.ComboBoxBase;
 import com.vaadin.flow.data.provider.ListDataProvider;
-import com.vaadin.flow.function.SerializableComparator;
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
 import io.github.northmaxdev.coinplot.backend.core.currency.CurrencyService;
+import io.github.northmaxdev.coinplot.frontend.common.ListDataProviders;
 import jakarta.annotation.Nonnull;
 
 import java.util.Comparator;
@@ -14,8 +14,7 @@ import java.util.Set;
 
 final class CurrencyPickers { // Package-private
 
-    // See SerializableComparator class JavaDoc for more information
-    private static final SerializableComparator<Currency> COMPARATOR = Comparator.comparing(Currency::getCode)::compare;
+    private static final Comparator<Currency> SORT_COMPARATOR = Comparator.comparing(Currency::getCode);
     // TODO: ItemFilter<Currency> that supports filtering by both the name OR the ISO code
 
     private CurrencyPickers() {
@@ -28,21 +27,17 @@ final class CurrencyPickers { // Package-private
         component.setAllowCustomValue(false);
     }
 
+    // TODO: How exactly do we want to react to "no data" cases? And do we really need this boolean return value for that?
     @SuppressWarnings("UnusedReturnValue") // Sometimes we may not care about the number of loaded items
     static <C extends ComboBoxBase<C, Currency, ?>> boolean loadItems(@Nonnull C component, @Nonnull CurrencyService dataSource) {
         // Don't null-check: this is a package-private class
         Set<Currency> availableCurrencies = dataSource.getAvailableCurrencies();
 
-        // There's no guarantee (in terms of API contracts) that CurrencyService::getAvailableCurrencies
-        // will return an immutable set, so make a protective copy just in case.
-        Set<Currency> copiedItems = Set.copyOf(availableCurrencies);
-
-        ListDataProvider<Currency> dataProvider = new ListDataProvider<>(copiedItems);
-        dataProvider.setSortComparator(COMPARATOR);
+        ListDataProvider<Currency> dataProvider = ListDataProviders.create(SORT_COMPARATOR, availableCurrencies);
         component.setItems(dataProvider);
 
         // Return 'true' if we loaded at least one item and 'false' if none at all.
         // This can help components adapt their visual appearance for "no data" cases.
-        return !copiedItems.isEmpty();
+        return !availableCurrencies.isEmpty();
     }
 }
