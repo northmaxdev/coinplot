715c75a9b2c56b8cad867fcfea7ad1d69ce04db3
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-24 10:58:42 +0300

    Add DataProviderPicker

---
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/DataProviderPicker.java
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.frontend.domain;
+
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
+import io.github.northmaxdev.coinplot.backend.core.DataProvider;
+import io.github.northmaxdev.coinplot.frontend.common.StrictRadioButtonGroup;
+import io.github.northmaxdev.coinplot.frontend.i18n.I18NUtilities;
+import jakarta.annotation.Nonnull;
+
+public final class DataProviderPicker extends StrictRadioButtonGroup<DataProvider> implements LocaleChangeObserver {
+
+    private static final String HELPER_TEXT_KEY = "data-provider-picker.helper-text";
+
+    public DataProviderPicker(@Nonnull DataProvider... items) {
+        super(items);
+        setItemLabelGenerator(DataProvider::getDisplayName);
+    }
+
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        I18NUtilities.setHelperText(this, event, HELPER_TEXT_KEY);
+    }
+}
