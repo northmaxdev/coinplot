208f033012ad5d5700d070aac227d801e1e10fa7
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-25 00:48:00 +0300

    DataProviderPicker: un-implement LocaleChangeObserver

--- src/main/java/io/github/northmaxdev/coinplot/frontend/domain/DataProviderPicker.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/DataProviderPicker.java
@@ -2,26 +2,19 @@
 
 package io.github.northmaxdev.coinplot.frontend.domain;
 
-import com.vaadin.flow.i18n.LocaleChangeEvent;
-import com.vaadin.flow.i18n.LocaleChangeObserver;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import io.github.northmaxdev.coinplot.backend.core.DataProvider;
-import io.github.northmaxdev.coinplot.frontend.common.StrictRadioButtonGroup;
-import io.github.northmaxdev.coinplot.frontend.i18n.I18NUtilities;
+import io.github.northmaxdev.coinplot.frontend.common.ListDataProviders;
 import jakarta.annotation.Nonnull;
 
 import java.util.Comparator;
 
-public final class DataProviderPicker extends StrictRadioButtonGroup<DataProvider> implements LocaleChangeObserver {
+public final class DataProviderPicker extends RadioButtonGroup<DataProvider> {
 
-    private static final String HELPER_TEXT_KEY = "data-provider-picker.helper-text";
     private static final Comparator<DataProvider> SORT_COMPARATOR = Comparator.comparing(DataProvider::getDisplayName);
 
     public DataProviderPicker(@Nonnull DataProvider... dataProviders) {
-        super(DataProvider::getDisplayName, SORT_COMPARATOR, dataProviders);
-    }
-
-    @Override
-    public void localeChange(LocaleChangeEvent event) {
-        I18NUtilities.setHelperText(this, event, HELPER_TEXT_KEY);
+        setItems(ListDataProviders.create(SORT_COMPARATOR, dataProviders));
+        setItemLabelGenerator(DataProvider::getDisplayName);
     }
 }
