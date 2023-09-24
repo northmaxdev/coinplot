cdde174ba8c76805918452ef7f0d3d22f38aa8dc
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-09-24 22:35:00 +0300

    ChronoUnitPicker: move to frontend.common and base on StrictSelect

--- src/main/java/io/github/northmaxdev/coinplot/frontend/common/ChronoUnitPicker.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/common/ChronoUnitPicker.java
@@ -1,46 +1,51 @@
 // SPDX-License-Identifier: MIT
 
-package io.github.northmaxdev.coinplot.frontend.core;
+package io.github.northmaxdev.coinplot.frontend.common;
 
-import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.Locale;
-import java.util.Set;
 
-public final class ChronoUnitPicker extends Select<ChronoUnit> implements LocaleChangeObserver {
+public final class ChronoUnitPicker extends StrictSelect<ChronoUnit> implements LocaleChangeObserver {
 
     public ChronoUnitPicker(@Nonnull ChronoUnit... units) {
-        this(Set.of(units)); // Implicit null-check(s)
-    }
-
-    public ChronoUnitPicker(@Nonnull Set<ChronoUnit> units) {
-        Set<ChronoUnit> copiedItems = Set.copyOf(units); // Implicit null-check(s)
-
-        ListDataProvider<ChronoUnit> dataProvider = new ListDataProvider<>(copiedItems);
-        dataProvider.setSortComparator(Enum::compareTo);
-        setItems(dataProvider);
-
+        // ChronoUnit::toString is a "throwaway" ItemLabelGenerator that we pass only
+        // because we cannot reference 'this' before supertype constructor is called.
+        super(ChronoUnit::toString, Comparator.naturalOrder(), units);
+        setItemLabelGenerator(this::getTranslatedUnitLabel);
         setEmptySelectionAllowed(false);
     }
 
+    ////////////////
+    // L10N stuff //
+    ////////////////
+
     @Override
     public void localeChange(@Nonnull LocaleChangeEvent event) {
-        setItemLabelGenerator(unit -> getTranslatedUnitLabel(unit, event.getLocale()));
+        setItemLabelGenerator(unit -> {
+            Locale locale = event.getLocale();
+            return getTranslatedUnitLabel(unit, locale);
+        });
+    }
+
+    private @Nonnull String getTranslatedUnitLabel(@Nullable ChronoUnit unit) {
+        return getTranslatedUnitLabel(unit, getLocale());
     }
 
     private @Nonnull String getTranslatedUnitLabel(@Nullable ChronoUnit unit, @Nonnull Locale locale) {
-        // No need to null-check locale
+        // No need to null-check locale, this method is used
+        // only in contexts where it is guaranteed to be not null.
+
         if (unit == null) {
             return "";
         }
 
-        String i18nKey = switch (unit) {
+        @Nonnull String propertyKey = switch (unit) {
             case NANOS -> "chrono-unit-picker.item-label.nanos";
             case MICROS -> "chrono-unit-picker.item-label.micros";
             case MILLIS -> "chrono-unit-picker.item-label.millis";
@@ -59,6 +64,6 @@ public final class ChronoUnitPicker extends Select<ChronoUnit> implements Locale
             case FOREVER -> "chrono-unit-picker.item-label.forever";
         };
 
-        return getTranslation(locale, i18nKey);
+        return getTranslation(locale, propertyKey);
     }
 }
