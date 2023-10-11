2fc0c3202043e83767bce87f427d9857b61cf5e6
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-11 16:24:04 +0300

    Implement DatelessExchange::toString

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/DatelessExchange.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/exchange/DatelessExchange.java
@@ -3,18 +3,14 @@
 package io.github.northmaxdev.coinplot.backend.core.exchange;
 
 import io.github.northmaxdev.coinplot.backend.core.currency.Currency;
-import io.github.northmaxdev.coinplot.lang.TextuallyDisplayable;
 import jakarta.annotation.Nonnull;
 
-import java.util.Locale;
 import java.util.Objects;
 
 // This class exists primarily for cases when we absolutely need to represent a currency exchange without a date.
 // One such scenario has to do with equals/hashCode semantics.
 // This class has been retrofitted into the Exchange/ExchangeRate class structure with no intentions of making it JPA-related.
-public record DatelessExchange(@Nonnull Currency base, @Nonnull Currency target) implements TextuallyDisplayable {
-
-    private static final String DISPLAY_NAME_SEPARATOR = " → ";
+public record DatelessExchange(@Nonnull Currency base, @Nonnull Currency target) {
 
     public DatelessExchange {
         Objects.requireNonNull(base);
@@ -30,13 +26,7 @@ public record DatelessExchange(@Nonnull Currency base, @Nonnull Currency target)
     }
 
     @Override
-    public @Nonnull String getDisplayName() {
-        return base.getDisplayName() + DISPLAY_NAME_SEPARATOR + target.getDisplayName();
-    }
-
-    @Override
-    public @Nonnull String getDisplayName(@Nonnull Locale locale) {
-        Objects.requireNonNull(locale);
-        return base.getDisplayName(locale) + DISPLAY_NAME_SEPARATOR + target.getDisplayName(locale);
+    public String toString() {
+        return base.getCode() + " → " + target.getCode();
     }
 }
