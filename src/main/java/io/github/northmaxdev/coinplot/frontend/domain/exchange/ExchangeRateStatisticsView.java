0ff1f8f17e9e0d275ad87c0623cbfb8815852d81
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-12 17:54:09 +0300

    ExchangeRateStatisticsView: add support for earliest and latest values

--- src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/ExchangeRateStatisticsView.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/ExchangeRateStatisticsView.java
@@ -18,25 +18,33 @@ public final class ExchangeRateStatisticsView extends HorizontalLayout implement
     private static final String MIN_VALUE_FIELD_LABEL_KEY = "exchange-rate-statistic-view.min-value-field.label";
     private static final String MAX_VALUE_FIELD_LABEL_KEY = "exchange-rate-statistic-view.max-value-field.label";
     private static final String AVERAGE_VALUE_FIELD_LABEL_KEY = "exchange-rate-statistic-view.average-value-field.label";
+    private static final String EARLIEST_VALUE_FIELD_LABEL_KEY = "exchange-rate-statistic-view.earliest-value-field.label";
+    private static final String LATEST_VALUE_FIELD_LABEL_KEY = "exchange-rate-statistic-view.latest-value-field.label";
     private static final String VALUE_COUNT_FIELD_LABEL_KEY = "exchange-rate-statistic-view.value-count-field.label";
 
     private final BigDecimalField minValueField;
     private final BigDecimalField maxValueField;
     private final BigDecimalField averageValueField;
+    private final BigDecimalField earliestValueField;
+    private final BigDecimalField latestValueField;
     private final IntegerField valueCountField;
 
     public ExchangeRateStatisticsView() {
         minValueField = new BigDecimalField();
         maxValueField = new BigDecimalField();
         averageValueField = new BigDecimalField();
+        earliestValueField = new BigDecimalField();
+        latestValueField = new BigDecimalField();
         valueCountField = new IntegerField();
 
         minValueField.setReadOnly(true);
         maxValueField.setReadOnly(true);
         averageValueField.setReadOnly(true);
+        earliestValueField.setReadOnly(true);
+        latestValueField.setReadOnly(true);
         valueCountField.setReadOnly(true);
 
-        add(minValueField, maxValueField, averageValueField, valueCountField);
+        add(minValueField, maxValueField, averageValueField, earliestValueField, latestValueField, valueCountField);
     }
 
     public void visualize(@Nonnull ExchangeRateStatistics statistics) {
@@ -44,11 +52,13 @@ public final class ExchangeRateStatisticsView extends HorizontalLayout implement
 
         statistics.getMinValue()
                 .ifPresentOrElse(minValueField::setValue, minValueField::clear);
-
         statistics.getMaxValue()
                 .ifPresentOrElse(maxValueField::setValue, maxValueField::clear);
-
         averageValueField.setValue(statistics.getAverageValue());
+        statistics.getEarliestValue()
+                .ifPresentOrElse(earliestValueField::setValue, earliestValueField::clear);
+        statistics.getLatestValue()
+                .ifPresentOrElse(latestValueField::setValue, latestValueField::clear);
         valueCountField.setValue(statistics.getValueCount());
     }
 
@@ -56,6 +66,8 @@ public final class ExchangeRateStatisticsView extends HorizontalLayout implement
         minValueField.clear();
         maxValueField.clear();
         averageValueField.clear();
+        earliestValueField.clear();
+        latestValueField.clear();
         valueCountField.clear();
     }
 
@@ -69,6 +81,12 @@ public final class ExchangeRateStatisticsView extends HorizontalLayout implement
 
         averageValueField.setLocale(event.getLocale());
         I18NUtilities.setLabel(averageValueField, event, AVERAGE_VALUE_FIELD_LABEL_KEY);
+
+        earliestValueField.setLocale(event.getLocale());
+        I18NUtilities.setLabel(earliestValueField, event, EARLIEST_VALUE_FIELD_LABEL_KEY);
+
+        latestValueField.setLocale(event.getLocale());
+        I18NUtilities.setLabel(latestValueField, event, LATEST_VALUE_FIELD_LABEL_KEY);
 
         // IntegerField does not have locale settings
         I18NUtilities.setLabel(valueCountField, event, VALUE_COUNT_FIELD_LABEL_KEY);
