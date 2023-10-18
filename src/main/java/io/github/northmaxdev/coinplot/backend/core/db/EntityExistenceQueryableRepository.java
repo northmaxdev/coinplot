1a0cb7ecee398743cadc95743416482bbf0b9c15
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-18 22:06:53 +0300

    EntityExistenceQueryableRepository: replace existAllById with percentageOfExistingEntities

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/db/EntityExistenceQueryableRepository.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/db/EntityExistenceQueryableRepository.java
@@ -2,7 +2,8 @@
 
 package io.github.northmaxdev.coinplot.backend.core.db;
 
-import io.github.northmaxdev.coinplot.lang.Iterables;
+import io.github.northmaxdev.coinplot.lang.math.Percentage;
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
@@ -10,19 +11,12 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface EntityExistenceQueryableRepository<T, I> extends CrudRepository<T, I> {
 
-    default boolean existsByNullableId(@Nullable I id) {
-        return id != null && existsById(id);
+    // TODO: Give this method a better name, preferably something that follows Spring Data naming conventions
+    default Percentage percentageOfExistingEntities(@Nonnull Iterable<I> ids) {
+        return Percentage.ofMatchingPredicate(ids, this::existsByNullableId);
     }
 
-    // A conceptual merge of the following methods:
-    //     boolean existsById(I);
-    //     Iterable<T> findAllById(Iterable<I>);
-    // Cases:
-    //     true -> If 'ids' is non-empty and ALL the elements exist within this repository
-    //     false -> If 'ids' is non-empty and NOT ALL the elements exist within this repository, if any
-    //     null -> If 'ids' is null or empty
-    default @Nullable Boolean existAllById(@Nullable Iterable<I> ids) {
-        return Iterables.isNullOrEmpty(ids) ? null : Iterables.stream(ids)
-                .allMatch(this::existsByNullableId);
+    default boolean existsByNullableId(@Nullable I id) {
+        return id != null && existsById(id);
     }
 }
