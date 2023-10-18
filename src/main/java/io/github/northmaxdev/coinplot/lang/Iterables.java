042b1adb80c2102c42d8af3fcab104b8555a8fe5
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-18 21:39:49 +0300

    Reorganize Iterables

--- src/main/java/io/github/northmaxdev/coinplot/lang/Iterables.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/Iterables.java
@@ -3,9 +3,9 @@
 package io.github.northmaxdev.coinplot.lang;
 
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -15,28 +15,46 @@ public final class Iterables {
         throw new UnsupportedOperationException();
     }
 
-    public static boolean isNullOrEmpty(@Nullable Iterable<?> iterable) {
+    // All methods are implemented using switch expressions and pattern-matching
+    // to allow for future extensibility of edge cases.
+
+    public static int size(@Nonnull Iterable<?> iterable) {
         return switch (iterable) {
-            case null -> true;
-            case Collection<?> collection -> collection.isEmpty();
-            // FIXME: Keep in mind that one can only query whether an iterator has any elements *remaining*
-            default -> iterable.iterator().hasNext();
+            case null -> throw new NullPointerException();
+            case Collection<?> collection -> collection.size();
+            default -> {
+                int counter = 0;
+                for (Object ignored : iterable) counter++;
+                yield counter;
+            }
         };
     }
 
-    public static <T> @Nonnull Stream<T> stream(@Nullable Iterable<T> iterable) {
+    public static boolean isEmpty(@Nonnull Iterable<?> iterable) {
+        return switch (iterable) {
+            case null -> throw new NullPointerException();
+            case Collection<?> collection -> collection.isEmpty();
+            default -> {
+                // Keep in mind that one can only query whether an iterator has any elements *remaining*
+                Iterator<?> iterator = iterable.iterator();
+                yield iterator.hasNext();
+            }
+        };
+    }
+
+    public static <T> @Nonnull Stream<T> stream(@Nonnull Iterable<T> iterable) {
         return stream(iterable, false);
     }
 
-    public static <T> @Nonnull Stream<T> stream(@Nullable Iterable<T> iterable, boolean parallel) {
+    public static <T> @Nonnull Stream<T> stream(@Nonnull Iterable<T> iterable, boolean parallel) {
         return switch (iterable) {
-            case null -> Stream.empty();
+            case null -> throw new NullPointerException();
             case Collection<T> collection -> parallel ? collection.parallelStream() : collection.stream();
             default -> StreamSupport.stream(iterable.spliterator(), parallel);
         };
     }
 
-    public static <T> @Nonnull Stream<T> parallelStream(@Nullable Iterable<T> iterable) {
+    public static <T> @Nonnull Stream<T> parallelStream(@Nonnull Iterable<T> iterable) {
         return stream(iterable, true);
     }
 }
