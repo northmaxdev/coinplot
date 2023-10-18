80422ed9b224bc89eae474ab34fb3d38fddbaab6
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-18 23:41:21 +0300

    Add LocalDateInterval::toSortedSet

--- src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/chrono/LocalDateInterval.java
@@ -6,11 +6,16 @@ import jakarta.annotation.Nonnull;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Objects;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Stream;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
 
 // Endpoints: start is inclusive, end is exclusive
 // Mathematical notation: [start, end)
@@ -35,6 +40,14 @@ public record LocalDateInterval(@Nonnull LocalDate start, @Nonnull LocalDate end
 
     public @Nonnull Stream<LocalDate> stream() {
         return start.datesUntil(end);
+    }
+
+    public @Nonnull SortedSet<LocalDate> toSortedSet() {
+        return stream()
+                .collect(collectingAndThen(
+                        toCollection(TreeSet::new),
+                        Collections::unmodifiableSortedSet
+                ));
     }
 
     public int length() {
