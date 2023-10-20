b862bc06f9b090dc284b1c55f32d015f72f0cf7e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-20 11:19:33 +0300

    Add DataProviderConfiguration

--- src/main/java/io/github/northmaxdev/coinplot/CoinPlotApp.java
+++ src/main/java/io/github/northmaxdev/coinplot/CoinPlotApp.java
@@ -2,6 +2,7 @@
 
 package io.github.northmaxdev.coinplot;
 
+import io.github.northmaxdev.coinplot.backend.core.DataProviderConfiguration;
 import io.github.northmaxdev.coinplot.backend.fixer.FixerConfiguration;
 import io.github.northmaxdev.coinplot.backend.frankfurter.FrankfurterConfiguration;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties({
+        DataProviderConfiguration.class,
         FixerConfiguration.class,
         FrankfurterConfiguration.class
 })
