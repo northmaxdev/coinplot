daacc82db37c978388c6c4fc9d0c7289b63c79af
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-21 11:48:19 +0300

    Add AbstractExchangeRateSetChart

---
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/AbstractExchangeRateSetChart.java
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.frontend.domain.exchange;
+
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.Configuration;
+import com.vaadin.flow.component.charts.model.Series;
+import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRate;
+import jakarta.annotation.Nonnull;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class AbstractExchangeRateSetChart extends Chart {
+
+    //////////////////////////////////
+    // Protected API for subclasses //
+    //////////////////////////////////
+
+    protected AbstractExchangeRateSetChart(@Nonnull ChartType chartType) {
+        super(chartType);
+    }
+
+    protected abstract @Nonnull List<Series> createSeriesFromDataset(@Nonnull Set<ExchangeRate> dataset);
+
+    ////////////////
+    // Public API //
+    ////////////////
+
+    public final void visualize(@Nonnull Set<ExchangeRate> dataset) {
+        Objects.requireNonNull(dataset);
+        List<Series> series = createSeriesFromDataset(dataset);
+
+        Configuration chartConfig = getConfiguration();
+        chartConfig.setSeries(series);
+        drawChart(true); // FIXME: setSeries() JavaDoc says this needs to be called for redraws, check that later
+    }
+}
