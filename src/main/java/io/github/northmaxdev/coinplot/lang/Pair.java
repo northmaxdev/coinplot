7c553445a45cdb5f5c34634a98c0e65fa10f460b
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-22 13:26:31 +0300

    Add Pair

---
+++ src/main/java/io/github/northmaxdev/coinplot/lang/Pair.java
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.lang;
+
+import jakarta.annotation.Nonnull;
+
+import java.util.Objects;
+
+// If 'T' has well-defined equals/hashCode, then so does Pair<T>
+public record Pair<T>(@Nonnull T first, @Nonnull T second) {
+
+    public Pair(@Nonnull T first, @Nonnull T second) {
+        this.first = Objects.requireNonNull(first);
+        this.second = Objects.requireNonNull(second);
+    }
+}
