21b9e599e04f12b53938cd21d23df1d7f868337c
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-25 13:54:35 +0300

    Add Pair methods for predicate-matching

--- src/main/java/io/github/northmaxdev/coinplot/lang/Pair.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/Pair.java
@@ -5,6 +5,8 @@ package io.github.northmaxdev.coinplot.lang;
 import jakarta.annotation.Nonnull;
 
 import java.util.Objects;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 // It is strongly recommended for 'T' to have proper equals/hashCode
 // semantics for Pair<T> to function correctly.
@@ -25,5 +27,17 @@ public record Pair<T>(@Nonnull T first, @Nonnull T second) {
 
     public @Nonnull Pair<T> flipped() {
         return new Pair<>(second, first);
+    }
+
+    public boolean matches(@Nonnull BiPredicate<T, T> predicate) {
+        return predicate.test(first, second);
+    }
+
+    public boolean firstMatches(@Nonnull Predicate<T> predicate) {
+        return predicate.test(first);
+    }
+
+    public boolean secondMatches(@Nonnull Predicate<T> predicate) {
+        return predicate.test(second);
     }
 }
