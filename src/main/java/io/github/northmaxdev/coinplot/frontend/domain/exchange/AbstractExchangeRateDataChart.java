9f510431b67f43c844caa722f5e8864e256f54e3
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-10-25 16:47:12 +0300

    Generalize AbstractExchangeRateDataChart impl

--- src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/AbstractExchangeRateDataChart.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/AbstractExchangeRateDataChart.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.charts.model.ChartType;
 import com.vaadin.flow.component.charts.model.Configuration;
 import com.vaadin.flow.component.charts.model.Series;
+import io.github.northmaxdev.coinplot.backend.core.exchange.DatelessExchange;
 import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRateBatch;
 import jakarta.annotation.Nonnull;
 
@@ -23,10 +24,9 @@ public abstract class AbstractExchangeRateDataChart extends Chart {
         super(chartType);
     }
 
-    // Each batch should represent a single series, i.e., a one-to-one mapping.
-    // In terms of type semantics and interface contracts, we don't really need a List here,
-    // but it's what the Vaadin Charts API requires.
-    protected abstract @Nonnull List<Series> mapBatchesToSeries(@Nonnull Set<ExchangeRateBatch> exchangeRateBatches);
+    // The "IgnoringName" part means that the series name is configured outside this method,
+    // and therefore the implementing class should not care about it.
+    protected abstract @Nonnull Series mapBatchToSeriesIgnoringName(@Nonnull ExchangeRateBatch batch);
 
     ////////////////
     // Public API //
@@ -34,7 +34,17 @@ public abstract class AbstractExchangeRateDataChart extends Chart {
 
     public final void visualize(@Nonnull Set<ExchangeRateBatch> exchangeRateBatches) {
         Objects.requireNonNull(exchangeRateBatches);
-        List<Series> series = mapBatchesToSeries(exchangeRateBatches);
+
+        // In terms of type semantics and interface contracts, we don't really need a List here,
+        // but it's what the Vaadin Charts API requires.
+        List<Series> series = exchangeRateBatches.stream()
+                .map(batch -> {
+                    DatelessExchange exchange = batch.getExchange();
+                    Series s = mapBatchToSeriesIgnoringName(batch);
+                    s.setName(exchange.toString()); // Remember that toString() may not always be suitable for UI
+                    return s;
+                })
+                .toList();
 
         Configuration config = getConfiguration();
         config.setSeries(series);
