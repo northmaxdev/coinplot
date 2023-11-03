d8c3fa9f73cb061fa457b2f32221aaa8b545c325
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-03 22:48:54 +0200

    Null-check args in GeneralPurposePair

--- src/main/java/io/github/northmaxdev/coinplot/lang/GeneralPurposePair.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/GeneralPurposePair.java
@@ -4,7 +4,14 @@ package io.github.northmaxdev.coinplot.lang;
 
 import jakarta.annotation.Nonnull;
 
+import java.util.Objects;
+
 public record GeneralPurposePair<F, S>(@Nonnull F first, @Nonnull S second) implements Pair<F, S> {
+
+    public GeneralPurposePair(@Nonnull F first, @Nonnull S second) {
+        this.first = Objects.requireNonNull(first);
+        this.second = Objects.requireNonNull(second);
+    }
 
     @Override
     public @Nonnull F getFirst() {
