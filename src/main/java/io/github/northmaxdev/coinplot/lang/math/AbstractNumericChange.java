0a30f935791cdbd34583359ee20b3f5d247f9e49
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-11 16:31:41 +0200

    Revert AbstractNumericChange::hashCode to being abstract

--- src/main/java/io/github/northmaxdev/coinplot/lang/math/AbstractNumericChange.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/math/AbstractNumericChange.java
@@ -5,17 +5,13 @@ package io.github.northmaxdev.coinplot.lang.math;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
-import java.util.Objects;
-
 abstract class AbstractNumericChange<T extends Number> implements NumericChange<T> { // Package-private
 
     @Override
     public abstract boolean equals(@Nullable Object obj);
 
     @Override
-    public int hashCode() { // Non-final (default implementation)
-        return Objects.hash(getInitialValue(), getFinalValue());
-    }
+    public abstract int hashCode();
 
     @Override
     public final @Nonnull String toString() {
