f0ffb8a373d81464215afad73fcbd965988e568e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-12 15:18:51 +0200

    Remove label from AbstractTextualDisplay implementations
    This is to make the components more universal. Let enclosing components (e.g., forms) decide how to label their subcomponents.

--- src/main/java/io/github/northmaxdev/coinplot/frontend/domain/DataProviderTextualDisplay.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/DataProviderTextualDisplay.java
@@ -4,19 +4,12 @@ package io.github.northmaxdev.coinplot.frontend.domain;
 
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.i18n.LocaleChangeEvent;
-import com.vaadin.flow.i18n.LocaleChangeObserver;
 import io.github.northmaxdev.coinplot.backend.core.DataProvider;
 import io.github.northmaxdev.coinplot.frontend.common.AbstractTextualDisplay;
-import io.github.northmaxdev.coinplot.frontend.i18n.I18NUtilities;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
-public final class DataProviderTextualDisplay
-        extends AbstractTextualDisplay<DataProvider>
-        implements LocaleChangeObserver {
-
-    private static final String LABEL_KEY = "data-provider-textual-display.label";
+public final class DataProviderTextualDisplay extends AbstractTextualDisplay<DataProvider> {
 
     public DataProviderTextualDisplay() {
         super();
@@ -24,11 +17,6 @@ public final class DataProviderTextualDisplay
 
     public DataProviderTextualDisplay(@Nullable DataProvider item) {
         super(item);
-    }
-
-    @Override
-    public void localeChange(@Nonnull LocaleChangeEvent event) {
-        I18NUtilities.setLabel(this, event, LABEL_KEY);
     }
 
     @Override
