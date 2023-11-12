717b967b4c0a84a7080ce1f15fb7d6ff366e0098
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-12 02:24:09 +0200

    DataProviderService: default to no-op

--- src/main/java/io/github/northmaxdev/coinplot/backend/core/DataProviderService.java
+++ src/main/java/io/github/northmaxdev/coinplot/backend/core/DataProviderService.java
@@ -10,8 +10,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.SequencedMap;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toMap;
@@ -29,28 +27,10 @@ public final class DataProviderService {
                 .collect(toMap(DataProvider::getID, p -> p));
     }
 
-    public Optional<DataProvider> getSelectedProvider() {
-        return config.safelyGetSelectedProviderID()
-                .map(availableProviders::get);
-    }
-
-    // There's no explicit definition of how a default provider is chosen,
-    // but it'll generally just be the first one on the list.
-    public @Nonnull DataProvider getDefaultProvider() {
-        if (availableProviders instanceof SequencedMap<String, DataProvider> seq) {
-            var firstEntry = seq.firstEntry();
-            return firstEntry.getValue();
-        }
-
-        return availableProviders.values()
-                .stream()
-                .findFirst()
-                .orElseThrow(); // Will never happen
-    }
-
     public @Nonnull DataProvider getSelectedProviderOrDefault() {
-        return getSelectedProvider()
-                .orElseGet(this::getDefaultProvider);
+        return config.safelyGetSelectedProviderID()
+                .map(availableProviders::get)
+                .orElse(DataProvider.NO_OP);
     }
 
     public @Nonnull Collection<DataProvider> getAvailableProviders() {
