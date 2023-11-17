ccab5802e86a030cf5deed17cfda8aeea6b20fe2
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-17 15:22:31 +0200

    Remove batchPicker logic from ExchangeRateAnalyticsVisualizer

--- src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/ExchangeRateAnalyticsVisualizer.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/domain/exchange/ExchangeRateAnalyticsVisualizer.java
@@ -2,61 +2,42 @@
 
 package io.github.northmaxdev.coinplot.frontend.domain.exchange;
 
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
-import io.github.northmaxdev.coinplot.backend.core.exchange.DatelessExchange;
-import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRate;
 import io.github.northmaxdev.coinplot.backend.core.exchange.ExchangeRateBatch;
-import io.github.northmaxdev.coinplot.frontend.i18n.I18NUtilities;
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 
-import java.util.Set;
+import java.util.Objects;
 
-public final class ExchangeRateAnalyticsVisualizer extends VerticalLayout implements LocaleChangeObserver {
+public final class ExchangeRateAnalyticsVisualizer extends SplitLayout implements LocaleChangeObserver {
 
     private final ExchangeRateDynamicsChart dynamicsChart;
-    private final ExchangeRateBatchMetricsReport metricsReport;
-    private final Select<ExchangeRateBatch> batchPicker;
+    private final ExchangeRateMetricsReport metricsReport;
 
     public ExchangeRateAnalyticsVisualizer() {
         dynamicsChart = new ExchangeRateDynamicsChart();
-        metricsReport = new ExchangeRateBatchMetricsReport();
+        metricsReport = new ExchangeRateMetricsReport();
 
-        batchPicker = new Select<>();
-        batchPicker.setPrefixComponent(VaadinIcon.MONEY_EXCHANGE.create());
-        batchPicker.setItemLabelGenerator(batch -> {
-            DatelessExchange exchange = batch.getExchange();
-            return exchange.getLabel();
-        });
-        batchPicker.addValueChangeListener(event -> {
-            @Nullable ExchangeRateBatch selectedBatch = event.getValue();
-            if (selectedBatch == null) {
-                metricsReport.clear();
-            } else {
-                metricsReport.load(selectedBatch);
-            }
-        });
-
-        add(dynamicsChart, batchPicker, metricsReport);
-        setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
-        setJustifyContentMode(JustifyContentMode.BETWEEN);
+        addToPrimary(dynamicsChart);
+        addToSecondary(metricsReport);
     }
 
-    public void visualize(@Nonnull Set<ExchangeRate> dataset) {
-        Set<ExchangeRateBatch> batches = ExchangeRateBatch.multipleFromDataset(dataset);
+    public void visualize(@Nonnull ExchangeRateBatch batch) {
+        Objects.requireNonNull(batch);
 
-        dynamicsChart.visualize(batches);
-        batchPicker.setItems(batches);
+        dynamicsChart.plot(batch);
+        metricsReport.load(batch);
+    }
+
+    public void clear() {
+        dynamicsChart.clear();
+        metricsReport.clear();
     }
 
     @Override
     public void localeChange(@Nonnull LocaleChangeEvent event) {
         dynamicsChart.localeChange(event);
         metricsReport.localeChange(event);
-        I18NUtilities.setLabel(batchPicker, event, "exchange-rate-analytics-visualizer.batch-picker.label");
     }
 }
