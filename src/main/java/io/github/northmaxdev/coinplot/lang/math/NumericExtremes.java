2a9c0ff072a890829f52d8785471437dd1ed9945
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-18 14:28:25 +0200

    Add NumericExtremes::find

--- src/main/java/io/github/northmaxdev/coinplot/lang/math/NumericExtremes.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/math/NumericExtremes.java
@@ -2,12 +2,18 @@
 
 package io.github.northmaxdev.coinplot.lang.math;
 
+import io.github.northmaxdev.coinplot.lang.SequencedCollections;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.SequencedCollection;
 
+// https://www.statista.com/statistics-glossary/definition/204/extreme_value/
 public record NumericExtremes(@Nonnull BigDecimal min, @Nonnull BigDecimal max) {
 
     // We don't validate min and max against each other because
@@ -15,6 +21,21 @@ public record NumericExtremes(@Nonnull BigDecimal min, @Nonnull BigDecimal max) 
     public NumericExtremes(@Nonnull BigDecimal min, @Nonnull BigDecimal max) {
         this.min = Objects.requireNonNull(min);
         this.max = Objects.requireNonNull(max);
+    }
+
+    public static Optional<NumericExtremes> find(@Nonnull Collection<BigDecimal> values) {
+        return find(values, BigDecimal::compareTo);
+    }
+
+    public static Optional<NumericExtremes> find(@Nonnull Collection<BigDecimal> values, @Nonnull Comparator<BigDecimal> comparator) {
+        Objects.requireNonNull(values);
+        Objects.requireNonNull(comparator);
+
+        SequencedCollection<BigDecimal> sortedValues = values.stream()
+                .sorted(comparator)
+                .toList();
+
+        return SequencedCollections.applyToEndpoints(sortedValues, NumericExtremes::new);
     }
 
     @Override
