b354d777d00f06deff3d912f66da965006f21294
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-18 13:59:05 +0200

    Convert NumericExtremes into a Java record

--- src/main/java/io/github/northmaxdev/coinplot/lang/math/NumericExtremes.java
+++ src/main/java/io/github/northmaxdev/coinplot/lang/math/NumericExtremes.java
@@ -8,24 +8,13 @@ import jakarta.annotation.Nullable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public final class NumericExtremes {
-
-    private final @Nonnull BigDecimal min;
-    private final @Nonnull BigDecimal max;
+public record NumericExtremes(@Nonnull BigDecimal min, @Nonnull BigDecimal max) {
 
     // We don't validate min and max against each other because
     // the order might depend on custom Comparator rules.
     public NumericExtremes(@Nonnull BigDecimal min, @Nonnull BigDecimal max) {
         this.min = Objects.requireNonNull(min);
         this.max = Objects.requireNonNull(max);
-    }
-
-    public @Nonnull BigDecimal getMin() {
-        return min;
-    }
-
-    public @Nonnull BigDecimal getMax() {
-        return max;
     }
 
     @Override
@@ -38,10 +27,5 @@ public final class NumericExtremes {
     @Override
     public int hashCode() {
         return BigDecimals.hashIgnoringScale(min) ^ BigDecimals.hashIgnoringScale(max);
-    }
-
-    @Override
-    public @Nonnull String toString() {
-        return "[min=" + min + " max=" + max + ']';
     }
 }
