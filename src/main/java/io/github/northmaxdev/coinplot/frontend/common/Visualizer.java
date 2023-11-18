f876d93fd44f2ac660a2aacb5ce03bcc120a7ea5
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2023-11-19 00:04:19 +0200

    Add Visualizer::visualize for Optional

--- src/main/java/io/github/northmaxdev/coinplot/frontend/common/Visualizer.java
+++ src/main/java/io/github/northmaxdev/coinplot/frontend/common/Visualizer.java
@@ -4,9 +4,16 @@ package io.github.northmaxdev.coinplot.frontend.common;
 
 import jakarta.annotation.Nonnull;
 
+import java.util.Optional;
+
 public interface Visualizer<T> {
 
     void visualize(@Nonnull T obj);
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    default void visualize(Optional<T> optional) {
+        optional.ifPresentOrElse(this::visualize, this::clear);
+    }
 
     void clear();
 }
