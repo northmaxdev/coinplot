97ecea4d504b288d1db9048eee450418512bd077
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-03 16:57:30 +0200

    DatelessExchange: remove sugar

--- src/main/java/io/github/northmaxdev/coinplot/domain/DatelessExchange.java
+++ src/main/java/io/github/northmaxdev/coinplot/domain/DatelessExchange.java
@@ -14,15 +14,6 @@ public record DatelessExchange(Currency base, Currency target) {
         this.target = Objects.requireNonNull(target, "target must not be null");
     }
 
-    public static DatelessExchange of(Exchange exchange) {
-        // This is better to call than the constructor because it might cache
-        return exchange.withoutDate();
-    }
-
-    public static DatelessExchange of(ExchangeRate exchangeRate) {
-        return of(exchangeRate.getExchangeWithoutRate());
-    }
-
     @Override
     public String toString() {
         return base.getCurrencyCode() + Chars.RIGHT_DIRECTION_ARROW + target.getCurrencyCode();
