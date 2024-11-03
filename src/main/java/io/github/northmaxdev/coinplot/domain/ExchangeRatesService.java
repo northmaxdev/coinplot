565916ea3e82887bcac151f80893f612d167fa18
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-03 18:26:40 +0200

    Update ExchangeRatesService

---
+++ src/main/java/io/github/northmaxdev/coinplot/domain/ExchangeRatesService.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.domain;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Map;
+import java.util.Set;
+
+public interface ExchangeRatesService {
+
+    Set<Currency> getSupportedCurrencies();
+
+    Map<CurrencyExchange, BigDecimal> getExchangeRates(CurrencyExchangeBatch exchangesOfInterest);
+}
