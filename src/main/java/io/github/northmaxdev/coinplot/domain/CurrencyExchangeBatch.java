0ab39909359d4e51456381d48c3028202d294ea8
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-03 18:19:47 +0200

    Fix CurrencyExchangeBatch tests and doc

---
+++ src/main/java/io/github/northmaxdev/coinplot/domain/CurrencyExchangeBatch.java
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.domain;
+
+import io.github.northmaxdev.coinplot.util.LocalDateInterval;
+
+import java.util.Currency;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
+
+// A "compressed" way to represent a bunch of currency exchanges.
+// The following CurrencyExchange instances:
+//
+//     {FOO, BAR, 2000-01-01}
+//     {FOO, BAZ, 2000-01-01}
+//     {FOO, BAR, 2000-01-02}
+//     {FOO, BAZ, 2000-01-02}
+//
+// ...can be represented as the following CurrencyExchangeBatch:
+//
+//     {base=FOO, targets={BAR,BAZ}, dateInterval=[2000-01-01, 2000-01-03)}
+//
+// A CurrencyExchangeBatch easily converts into a sequence of CurrencyExchange instances, but not the other way around.
+// There will always be at least one target currency and one date (both non-null), so stream() is never empty.
+public record CurrencyExchangeBatch(Currency base, Set<Currency> targets, LocalDateInterval dateInterval) {
+
+    public CurrencyExchangeBatch(Currency base, Set<Currency> targets, LocalDateInterval dateInterval) {
+        this.base = Objects.requireNonNull(base);
+        this.dateInterval = Objects.requireNonNull(dateInterval);
+
+        if (targets.isEmpty()) { // Implicit null-check on the set itself
+            throw new IllegalArgumentException("must provide at least one target");
+        }
+        this.targets = Set.copyOf(targets); // Implicit null-checks on the set's items
+    }
+
+    public Stream<CurrencyExchange> stream() {
+        return dateInterval.stream().mapMulti((date, buffer) -> {
+            for (Currency target : targets) {
+                CurrencyExchange exchange = new CurrencyExchange(base, target, date);
+                buffer.accept(exchange);
+            }
+        });
+    }
+
+    public long size() {
+        return targets.size() * dateInterval.length();
+    }
+}
---
+++ src/test/java/io/github/northmaxdev/coinplot/domain/CurrencyExchangeBatchTests.java
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.domain;
+
+import io.github.northmaxdev.coinplot.util.LocalDateInterval;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Currency;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CurrencyExchangeBatchTests {
+
+    @Test
+    void stream() {
+        Currency euro = Currency.getInstance("EUR");
+        Currency swissFranc = Currency.getInstance("CHF");
+        Currency canadianDollar = Currency.getInstance("CAD");
+
+        CurrencyExchangeBatch batch = new CurrencyExchangeBatch(
+                euro,
+                Set.of(swissFranc, canadianDollar),
+                new LocalDateInterval(
+                        LocalDate.of(2000, Month.JANUARY, 1),
+                        LocalDate.of(2000, Month.JANUARY, 4)
+                )
+        );
+
+        Stream<CurrencyExchange> stream = batch.stream();
+
+        assertThat(stream).containsExactlyInAnyOrder(
+                new CurrencyExchange(euro, swissFranc, LocalDate.of(2000, Month.JANUARY, 1)),
+                new CurrencyExchange(euro, canadianDollar, LocalDate.of(2000, Month.JANUARY, 1)),
+                new CurrencyExchange(euro, swissFranc, LocalDate.of(2000, Month.JANUARY, 2)),
+                new CurrencyExchange(euro, canadianDollar, LocalDate.of(2000, Month.JANUARY, 2)),
+                new CurrencyExchange(euro, swissFranc, LocalDate.of(2000, Month.JANUARY, 3)),
+                new CurrencyExchange(euro, canadianDollar, LocalDate.of(2000, Month.JANUARY, 3))
+        );
+    }
+}
