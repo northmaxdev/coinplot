cd5c3969fa0e8b27466ef6588f747e293a99f670
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-06 19:16:15 +0200

    Add back Frankfurter DTO

---
+++ src/main/java/io/github/northmaxdev/coinplot/domain/currency/ExchangeRatesDto.java
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.domain.currency;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExchangeRatesDto(String base, Map<LocalDate, Map<String, BigDecimal>> rates) {
+}
