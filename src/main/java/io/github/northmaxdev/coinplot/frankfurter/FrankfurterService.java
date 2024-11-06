66c2f25bdf04e5f8ce41ecc957c6f890d186ead4
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-06 18:11:13 +0200

    Implement FrankfurterService::getSupportedCurrencies

---
+++ src/main/java/io/github/northmaxdev/coinplot/frankfurter/FrankfurterService.java
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+package io.github.northmaxdev.coinplot.frankfurter;
+
+import io.github.northmaxdev.coinplot.domain.CurrencyExchange;
+import io.github.northmaxdev.coinplot.domain.CurrencyExchangeBatch;
+import io.github.northmaxdev.coinplot.domain.ExchangeRatesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toUnmodifiableSet;
+
+@Service
+public final class FrankfurterService implements ExchangeRatesService {
+
+    private final RestClient restClient;
+
+    @Autowired
+    public FrankfurterService(FrankfurterConfig config) {
+        this.restClient = RestClient.create(config.host());
+    }
+
+    @Override
+    public Set<Currency> getSupportedCurrencies() {
+        Map<String, String> response = restClient.get()
+                .uri("/currencies")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        if (response == null) {
+            throw new IllegalStateException("DTO is null");
+        }
+
+        return response.keySet()
+                .stream()
+                .map(Currency::getInstance)
+                .collect(toUnmodifiableSet());
+    }
+
+    @Override
+    public Map<CurrencyExchange, BigDecimal> getExchangeRates(CurrencyExchangeBatch exchangesOfInterest) {
+        return Map.of();
+    }
+}
