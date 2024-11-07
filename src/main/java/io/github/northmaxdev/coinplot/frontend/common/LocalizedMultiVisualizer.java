6c4b6afe804a96c33f08da0562e9ab86893a02c3
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-07 22:09:23 +0200

    remove unneeded ui stuff

--- src/main/java/io/github/northmaxdev/coinplot/frontend/common/LocalizedMultiVisualizer.java
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-package io.github.northmaxdev.coinplot.frontend.common;
-
-import com.vaadin.flow.i18n.LocaleChangeObserver;
-
-public interface LocalizedMultiVisualizer<T> extends MultiVisualizer<T>, LocaleChangeObserver {}
