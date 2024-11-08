51380ed6c21192d072558940d77c82704f8c3fc7
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-11-09 00:44:37 +0200

    Remove custom input fields

--- src/main/java/io/github/northmaxdev/coinplot/ui/fields/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-@NonNullApi
-@NonNullFields
-package io.github.northmaxdev.coinplot.ui.fields;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
