b28c72f5782e8ea5f77375998e1739fbbf70c2cf
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-12 00:04:40 +0300

    Add JavaDoc to DatelessCurrencyExchange

--- src/main/java/io/github/northmaxdev/coinplot/domain/DatelessCurrencyExchange.java
+++ src/main/java/io/github/northmaxdev/coinplot/domain/DatelessCurrencyExchange.java
@@ -4,16 +4,47 @@ package io.github.northmaxdev.coinplot.domain;
 
 import java.util.Currency;
 import java.util.Objects;
+import java.util.function.Function;
 
+
+/**
+ * This class exists for when we need to map just the exchange itself (no date) to something.
+ * <p>Example:</p>
+ * <pre>{@code
+ * Map<CurrencyExchange, BigDecimal>
+ *
+ * {
+ *     [USD, CHF, 2019-12-31] : 0.96617
+ *     [USD, EUR, 2019-12-31] : 0.89015
+ *     [USD, CHF, 2020-12-31] : 0.88029
+ *     [USD, EUR, 2020-12-31] : 0.81493
+ * }}
+ * </pre>
+ * <p>
+ * to:
+ * <p>
+ * <pre>{@code
+ * Map<DatelessCurrencyExchange, Collection<T>>
+ *
+ * {
+ *     [USD, CHF] : (T, T, ...)
+ *     [USD, EUR] : (T, T, ...)
+ * }}
+ * </pre>
+ * <p>where the date and exchange rate value get mapped to a bunch of {@code T}'s.</p>
+ * <p>Needless to say, {@code equals}/{@code hashCode} correctness are critical here.</p>
+ *
+ * @see java.util.stream.Collectors#groupingBy(Function)
+ */
 public record DatelessCurrencyExchange(Currency base, Currency target) {
 
-    public DatelessCurrencyExchange(Currency base, Currency target) {
-        this.base = Objects.requireNonNull(base, "base must not be null");
-        this.target = Objects.requireNonNull(target, "target must not be null");
+    public DatelessCurrencyExchange {
+        Objects.requireNonNull(base, "base must not be null");
+        Objects.requireNonNull(target, "target must not be null");
     }
 
     @Override
     public String toString() {
-        return base.getCurrencyCode() + '→' + target.getCurrencyCode();
+        return base.getCurrencyCode() + "-->" + target.getCurrencyCode();
     }
 }
