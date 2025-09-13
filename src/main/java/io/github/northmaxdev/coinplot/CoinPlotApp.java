5bfdaf2dbeffccaf87feab1c2428239bc2717f4a
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-14 01:01:04 +0300

    Set en_GB as main locale of the JVM

--- src/main/java/io/github/northmaxdev/coinplot/CoinPlotApp.java
+++ src/main/java/io/github/northmaxdev/coinplot/CoinPlotApp.java
@@ -7,11 +7,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.util.Locale;
+
 @EnableConfigurationProperties({FrankfurterConfig.class})
 @SpringBootApplication
 public class CoinPlotApp {
 
+    public static final Locale COINPLOT_LOCALE = Locale.UK; // This is the closest thing to Microsoft's "English (International)"
+
     public static void main(String[] args) {
+        Locale.setDefault(COINPLOT_LOCALE);
         SpringApplication.run(CoinPlotApp.class, args);
     }
 }
