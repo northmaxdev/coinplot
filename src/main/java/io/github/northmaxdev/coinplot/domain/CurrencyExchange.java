b5efa287b7390dc98c4a7f4e8df55567b041282f
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-13 16:22:53 +0300

    CurrencyExchange: use Europe/Berlin zone ID instead of UTC+1 offset

--- src/main/java/io/github/northmaxdev/coinplot/domain/CurrencyExchange.java
+++ src/main/java/io/github/northmaxdev/coinplot/domain/CurrencyExchange.java
@@ -5,35 +5,35 @@ package io.github.northmaxdev.coinplot.domain;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Currency;
 import java.util.Objects;
 
 public record CurrencyExchange(Currency base, Currency target, LocalDate date) {
 
+    // https://data.ecb.europa.eu/methodology/exchange-rates
     private static final LocalTime USUAL_ECB_PUBLICATION_TIME = LocalTime.of(16, 0);
-    private static final ZoneOffset ECB_PUBLICATION_ZONE_OFFSET = ZoneOffset.ofHours(1); // CET
+    private static final ZoneId ECB_TIMEZONE = ZoneId.of("Europe/Berlin");
 
-    public CurrencyExchange(Currency base, Currency target, LocalDate date) {
-        this.base = Objects.requireNonNull(base, "base must not be null");
-        this.target = Objects.requireNonNull(target, "target must not be null");
-        this.date = Objects.requireNonNull(date, "date must not be null");
+    public CurrencyExchange {
+        Objects.requireNonNull(base, "base must not be null");
+        Objects.requireNonNull(target, "target must not be null");
+        Objects.requireNonNull(date, "date must not be null");
     }
 
     public DatelessCurrencyExchange withoutDate() {
-        // This can be cached
         return new DatelessCurrencyExchange(base, target);
     }
 
     public Instant approximatePublicationTimestamp() {
-        // This does not cover the case of CET/CEST transitions
         return date.atTime(USUAL_ECB_PUBLICATION_TIME)
-                .toInstant(ECB_PUBLICATION_ZONE_OFFSET);
+                .atZone(ECB_TIMEZONE)
+                .toInstant();
     }
 
     @Override
     public String toString() {
-        return base.getCurrencyCode() + '→' + target.getCurrencyCode() + ' ' + date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        return base.getCurrencyCode() + '/' + target.getCurrencyCode() + ' ' + date.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
