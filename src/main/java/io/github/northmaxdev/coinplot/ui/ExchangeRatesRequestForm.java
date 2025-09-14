8759e714012b24d6f6a8b2d5fa62a372758e5928
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-14 21:50:07 +0300

    Update ExchangeRatesRequestForm

--- src/main/java/io/github/northmaxdev/coinplot/ui/ExchangeRatesRequestForm.java
+++ src/main/java/io/github/northmaxdev/coinplot/ui/ExchangeRatesRequestForm.java
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-package io.github.northmaxdev.coinplot.ui.panels;
+package io.github.northmaxdev.coinplot.ui;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -14,7 +12,6 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.theme.lumo.LumoIcon;
 import io.github.northmaxdev.coinplot.domain.CurrencyExchangeBatch;
 import io.github.northmaxdev.coinplot.domain.ExchangeRatesService;
-import io.github.northmaxdev.coinplot.ui.ComponentL10n;
 import io.github.northmaxdev.coinplot.util.LocalDateInterval;
 import org.springframework.lang.Nullable;
 
@@ -25,94 +22,85 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 
+// TODO: Set minimum width for the entire layout
+// TODO: Add duration preview to form via LocalDateInterval::toPeriod
 public final class ExchangeRatesRequestForm extends FormLayout {
 
-    // TODO: Set minimum width for the entire layout
-    // TODO: Add duration preview to form via LocalDateInterval::toPeriod
-
-    private static final int CURRENCY_PICKER_COLSPAN = 2;
     private static final List<ResponsiveStep> RESPONSIVE_STEPS = List.of(
             new ResponsiveStep("0", 1),
             new ResponsiveStep("350px", 2)
     );
+    private static final int CURRENCY_PICKER_COLSPAN = 2;
 
-    private final ComboBox<Currency> basePicker;
-    private final MultiSelectComboBox<Currency> targetPicker;
+    private final CurrencyComboBox basePicker;
+    private final MultiCurrencyComboBox targetPicker;
     private final DatePicker startDatePicker;
     private final DatePicker endDatePicker;
     private @Nullable Consumer<CurrencyExchangeBatch> onSubmit;
 
     public ExchangeRatesRequestForm(ExchangeRatesService currencyDataSource) {
 
-        //////////////////////
+        //------------------//
         // Currency pickers //
-        //////////////////////
+        //------------------//
 
+        // TODO: load async
         Set<Currency> supportedCurrencies = currencyDataSource.getSupportedCurrencies();
 
-        this.basePicker = new ComboBox<>("מטבע המקור", supportedCurrencies);
-        this.basePicker.setTooltipText("המטבע שאתם נותנים בהמרה");
-        this.basePicker.setRequired(true);
-        ComponentL10n.localize(this.basePicker);
-        this.basePicker.setItemLabelGenerator(ComponentL10n.CURRENCY_LABEL_GENERATOR);
-        setColspan(this.basePicker, CURRENCY_PICKER_COLSPAN);
+        basePicker = new CurrencyComboBox("Base currency", supportedCurrencies);
+        basePicker.setRequired(true);
+        setColspan(basePicker, CURRENCY_PICKER_COLSPAN);
 
-        this.targetPicker = new MultiSelectComboBox<>("מטבע היעד", supportedCurrencies);
-        this.targetPicker.setTooltipText("המטבע שאתם מקבלים בהמרה");
-        this.targetPicker.setRequired(true);
-        ComponentL10n.localize(this.targetPicker);
-        this.targetPicker.setItemLabelGenerator(ComponentL10n.CURRENCY_LABEL_GENERATOR);
-        setColspan(this.targetPicker, CURRENCY_PICKER_COLSPAN);
+        targetPicker = new MultiCurrencyComboBox("Target currencies", supportedCurrencies);
+        targetPicker.setRequired(true);
+        targetPicker.setHelperText("You can select multiple currencies");
+        setColspan(targetPicker, CURRENCY_PICKER_COLSPAN);
 
-        //////////////////
+        //--------------//
         // Date pickers //
-        //////////////////
+        //--------------//
 
         LocalDate today = LocalDate.now();
-        LocalDate yesterday = today.minusDays(1L);
+        LocalDate yesterday = today.minusDays(1);
 
-        this.startDatePicker = new DatePicker("מתאריך");
-        this.startDatePicker.setRequired(true);
-        this.startDatePicker.setMax(yesterday);
-        ComponentL10n.localize(this.startDatePicker);
+        startDatePicker = new DatePicker("Start date");
+        startDatePicker.setRequired(true);
+        startDatePicker.setMax(yesterday);
 
-        this.endDatePicker = new DatePicker("עד לתאריך");
-        this.endDatePicker.setRequired(true);
-        this.endDatePicker.setMax(today);
-        ComponentL10n.localize(this.endDatePicker);
+        endDatePicker = new DatePicker("End date");
+        endDatePicker.setRequired(true);
+        endDatePicker.setMax(today);
 
-        this.startDatePicker.addValueChangeListener(event -> {
+        startDatePicker.addValueChangeListener(event -> {
             LocalDate start = event.getValue(); // Nullable (Spring nullability annotations don't support local variables)
-            LocalDate minEnd = (start == null) ? null : start.plusDays(1L);
-            this.endDatePicker.setMin(minEnd);
+            LocalDate minEnd = (start == null) ? null : start.plusDays(1);
+            endDatePicker.setMin(minEnd);
         });
 
-        this.endDatePicker.addValueChangeListener(event -> {
+        endDatePicker.addValueChangeListener(event -> {
             LocalDate end = event.getValue(); // Nullable (Spring nullability annotations don't support local variables)
-            LocalDate maxStart = (end == null) ? yesterday : end.minusDays(1L);
-            this.startDatePicker.setMax(maxStart);
+            LocalDate maxStart = (end == null) ? yesterday : end.minusDays(1);
+            startDatePicker.setMax(maxStart);
         });
 
-        /////////////
+        //---------//
         // Buttons //
-        /////////////
+        //---------//
 
-        Button okButton = new Button("אישור", LumoIcon.CHECKMARK.create());
+        Button okButton = new Button("Submit", LumoIcon.CHECKMARK.create());
         okButton.addSingleClickListener(_ -> submit());
         okButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         okButton.addClickShortcut(Key.ENTER);
-        okButton.setTooltipText("ניתן גם ללחוץ \"Enter\" בתור מקש קיצור");
-        this.onSubmit = null;
+        okButton.setTooltipText("Keyboard hotkey: Enter");
+        onSubmit = null;
 
-        Button clearButton = new Button("איפוס", LumoIcon.CROSS.create());
+        Button clearButton = new Button("Clear", LumoIcon.CROSS.create());
         clearButton.addSingleClickListener(_ -> clear());
         clearButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
-        clearButton.addClickShortcut(Key.DELETE);
-        clearButton.setTooltipText("ניתן גם ללחוץ \"Delete\" בתור מקש קיצור");
 
-        ////////////
+        //--------//
         // Layout //
-        ////////////
+        //--------//
 
         // https://vaadin.com/docs/latest/components/form-layout#button-placement
         HorizontalLayout buttonBar = new HorizontalLayout(okButton, clearButton);
@@ -120,7 +108,7 @@ public final class ExchangeRatesRequestForm extends FormLayout {
         buttonBar.setPadding(false); // This is so it's horizontally in line with the rest of the form's components
         buttonBar.setMargin(true);
 
-        add(this.basePicker, this.targetPicker, this.startDatePicker, this.endDatePicker, buttonBar);
+        add(basePicker, targetPicker, startDatePicker, endDatePicker, buttonBar);
         setResponsiveSteps(RESPONSIVE_STEPS);
     }
 
@@ -161,9 +149,9 @@ public final class ExchangeRatesRequestForm extends FormLayout {
     }
 
     public void clear() {
-        this.basePicker.clear();
-        this.targetPicker.clear();
-        this.startDatePicker.clear();
-        this.endDatePicker.clear();
+        basePicker.clear();
+        targetPicker.clear();
+        startDatePicker.clear();
+        endDatePicker.clear();
     }
 }
