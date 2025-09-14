98241354a1f73ae04730d47283ede89ebfa1e791
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-14 23:54:52 +0300

    Remove ui\panels dir

--- src/main/java/io/github/northmaxdev/coinplot/ui/panels/package-info.java
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-@NonNullApi
-@NonNullFields
-package io.github.northmaxdev.coinplot.ui.panels;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
