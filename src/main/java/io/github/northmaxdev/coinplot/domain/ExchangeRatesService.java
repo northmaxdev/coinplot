e3c3d130b367a047842387d3cc098b607be9c7d8
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-17 00:54:34 +0300

    Update ExchangeRatesService::getExchangeRates param and return type

--- src/main/java/io/github/northmaxdev/coinplot/domain/ExchangeRatesService.java
+++ src/main/java/io/github/northmaxdev/coinplot/domain/ExchangeRatesService.java
@@ -2,14 +2,12 @@
 
 package io.github.northmaxdev.coinplot.domain;
 
-import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.Map;
 import java.util.Set;
 
 public interface ExchangeRatesService {
 
     Set<Currency> getSupportedCurrencies();
 
-    Map<DatedExchange, BigDecimal> getExchangeRates(CurrencyExchangeBatch exchangesOfInterest);
+    ExchangeRatesDataset getExchangeRates(DatedExchangeZip exchangesOfInterest);
 }
