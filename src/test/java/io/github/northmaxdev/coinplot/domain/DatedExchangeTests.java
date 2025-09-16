867cb4531ae6eb448a915ca57b5110610d375b32
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2025-09-17 00:48:33 +0300

    Omit custom display names in Exchange and DatedExchange tests

--- src/test/java/io/github/northmaxdev/coinplot/domain/DatedExchangeTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/domain/DatedExchangeTests.java
@@ -3,14 +3,12 @@
 package io.github.northmaxdev.coinplot.domain;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class DatedExchangeTests {
 
     @Test
-    @DisplayName("equals/hashCode contract")
-    void equalsAndHashCode() { // Critical
+    void equalsAndHashCodeContract() { // Critical
         EqualsVerifier.forClass(DatedExchange.class).verify();
     }
 }
--- src/test/java/io/github/northmaxdev/coinplot/domain/ExchangeTests.java
+++ src/test/java/io/github/northmaxdev/coinplot/domain/ExchangeTests.java
@@ -3,14 +3,12 @@
 package io.github.northmaxdev.coinplot.domain;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ExchangeTests {
 
     @Test
-    @DisplayName("equals/hashCode contract")
-    void equalsAndHashCode() { // Critical
+    void equalsAndHashCodeContract() { // Critical
         EqualsVerifier.forClass(Exchange.class).verify();
     }
 }
